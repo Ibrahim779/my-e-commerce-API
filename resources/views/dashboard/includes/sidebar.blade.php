66e5beb45043c820d684f9cf7996f152a19cec18
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-13 13:23:59 +0200

    Update sidebar.blade.php

--- resources/views/dashboard/includes/sidebar.blade.php
+++ resources/views/dashboard/includes/sidebar.blade.php
@@ -12,22 +12,50 @@
 
                 <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi-clipboard-text"></i><span class="hide-menu">subcategories </span></a>
                     <ul aria-expanded="false" class="collapse  first-level">
-                        <li class="sidebar-item"><a href="{{route('subcategories.categories.index',1)}}" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Milk Products </span></a></li>
+                        @foreach($categories as $category)
+                        <li class="sidebar-item">
+                            <a href="{{route('subcategories.categories.index', $category->id)}}" class="sidebar-link">
+                                <i class="mdi mdi-note-outline"></i>
+                                <span class="hide-menu">{{$category->name}} </span>
+                            </a>
+                        </li>
+                            @endforeach
                     </ul>
                 </li>
                 <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-face"></i><span class="hide-menu">Products</span></a>
                     <ul aria-expanded="false" class="collapse  first-level">
-                        <li class="sidebar-item"><a href="{{route('products.categories.index',1)}}" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Milk Products </span></a></li>
+                        @foreach($categories as $category)
+                            <li class="sidebar-item">
+                                <a href="{{route('products.categories.index', $category->id)}}" class="sidebar-link">
+                                    <i class="mdi mdi-note-outline"></i>
+                                    <span class="hide-menu">{{$category->name}} </span>
+                                </a>
+                            </li>
+                        @endforeach
                     </ul>
                 </li>
                 <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-alert"></i><span class="hide-menu">Products UnPublished </span></a>
                     <ul aria-expanded="false" class="collapse  first-level">
-                        <li class="sidebar-item"><a href="{{route('products.categories.UnPublished',1)}}" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Milk Products </span></a></li>
+                        @foreach($categories as $category)
+                            <li class="sidebar-item">
+                                <a href="{{route('products.categories.unPublished', $category->id)}}" class="sidebar-link">
+                                    <i class="mdi mdi-note-outline"></i>
+                                    <span class="hide-menu">{{$category->name}} </span>
+                                </a>
+                            </li>
+                        @endforeach
                     </ul>
                 </li>
                 <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-alert"></i><span class="hide-menu">Products Has Discount</span></a>
                     <ul aria-expanded="false" class="collapse  first-level">
-                        <li class="sidebar-item"><a href="{{route('products.categories.hasDiscount',1)}}" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Milk Products </span></a></li>
+                        @foreach($categories as $category)
+                            <li class="sidebar-item">
+                                <a href="{{route('products.categories.hasDiscount', $category->id)}}" class="sidebar-link">
+                                    <i class="mdi mdi-note-outline"></i>
+                                    <span class="hide-menu">{{$category->name}} </span>
+                                </a>
+                            </li>
+                        @endforeach
                     </ul>
                 </li>
                 <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{route('products.isOffer')}}" aria-expanded="false"><i class="mdi mdi-chart-bar"></i><span class="hide-menu">Offers</span></a></li>
