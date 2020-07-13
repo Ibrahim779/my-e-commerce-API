9904cc2f1bc2840dd67e1607d31c256fa9203018
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-13 13:28:10 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,26 +15,33 @@ use Illuminate\Support\Facades\Route;
 Route::namespace('Dashboard')->prefix('dashboard')->group(function (){
     Route::get('/','DashboardController@index')->name('dashboard.index');
     Route::resource('categories', 'CategoryController')->except('show');
-    Route::resource('products', 'ProductController')->only(['destroy', 'update']);
     Route::name('products.')->prefix('products')->group(function (){
         Route::get('offers', 'ProductController@isOffer')->name('isOffer');
         Route::name('categories.')->prefix('categories')->group(function (){
             Route::get('{category}', 'ProductController@index')->name('index');
-            Route::get('{category}/unpublished', 'ProductController@getUnPublished')->name('UnPublished');
+            Route::get('{category}/unpublished', 'ProductController@getUnPublished')->name('unPublished');
             Route::get('{category}/has-discount', 'ProductController@getHasDiscount')->name('hasDiscount');
             Route::post('{category}', 'ProductController@store')->name('store');
+            Route::get('{category}/create', 'ProductController@create')->name('create');
+            Route::get('{category}/edit/{product}', 'ProductController@edit')->name('edit');
+            Route::patch('{category}/{product}', 'ProductController@update')->name('update');
+            Route::get('{category}/destroy/{product}', 'ProductController@destroy')->name('destroy');
         });
     });
     Route::name('subcategories.')->prefix('subcategories')->group(function (){
         Route::name('categories.')->prefix('categories')->group(function (){
             Route::get('{category}', 'SubCategoryController@index')->name('index');
             Route::post('{category}', 'SubCategoryController@store')->name('store');
+            Route::get('{category}/create', 'SubCategoryController@create')->name('create');
+            Route::get('{category}/edit/{subcategory}', 'SubCategoryController@edit')->name('edit');
+            Route::patch('{category}/{subcategory}', 'SubCategoryController@update')->name('update');
+            Route::get('{category}/destroy/{subcategory}', 'SubCategoryController@destroy')->name('destroy');
         });
     });
     Route::get('coupons',function (){
         return 'welcome';
     })->name('coupons.index');
-    Route::resource('subcategories', 'SubCategoryController')->only('destroy', 'update');
+
     Route::resource('brands', 'BrandController');
     Route::resource('sliders', 'SliderController');
     Route::resource('messages', 'MessageController')->only(['index','destroy']);
