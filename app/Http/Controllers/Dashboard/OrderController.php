6a8b10d12b7eb34b071fcb0bd27200f70ad41dae
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-18 15:08:04 +0200

    Update OrderController.php

--- app/Http/Controllers/Dashboard/OrderController.php
+++ app/Http/Controllers/Dashboard/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Order;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,10 +10,17 @@ class OrderController extends Controller
 {
     public function index()
     {
-
+        $orders = Order::with('user')->get();
+        return view('dashboard.order.index',compact('orders'));
     }
-    public function destroy()
+    public function show(Order $order)
     {
-
+        return view('dashboard.order.show',compact('order'));
     }
+    public function destroy(Order $order)
+    {
+       $order->delete();
+       return redirect()->back();
+    }
+
 }
