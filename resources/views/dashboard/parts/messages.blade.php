8912bc4582c6d671c6b889a9dae31242bf7452b7
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-18 15:01:28 +0200

    Update messages.blade.php

--- resources/views/dashboard/parts/messages.blade.php
+++ resources/views/dashboard/parts/messages.blade.php
@@ -6,7 +6,7 @@
         <!-- Comment Row -->
         @forelse($latest_messages as $latest_message)
         <div class="d-flex flex-row comment-row m-t-0">
-            <div class="p-2"><img src="../../assets/images/users/1.jpg" alt="user" width="50" class="rounded-circle"></div>
+            <div class="p-2"><img src="../../assets/dashboard/images/users/1.jpg" alt="user" width="50" class="rounded-circle"></div>
             <div class="comment-text w-100">
                 <h6 class="font-medium">{{$latest_message->name}}</h6>
                 <span class="m-b-15 d-block">{{$latest_message->message}} </span>
