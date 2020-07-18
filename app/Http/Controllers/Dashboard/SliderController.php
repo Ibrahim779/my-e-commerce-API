84c2dc17faea46d92804094eb783056c7b9fb76e
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-18 15:07:58 +0200

    Update SliderController.php

--- app/Http/Controllers/Dashboard/SliderController.php
+++ app/Http/Controllers/Dashboard/SliderController.php
@@ -18,39 +18,52 @@ class SliderController extends Controller
          $sliders = Slider::get();
         return view('dashboard.slider.index', compact('sliders'));
     }
+     public function create()
+     {
+         return view('dashboard.slider.create');
+     }
 
     /**
      * Store a newly created resource in storage.
      * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request)
     {
         $this->validation($request);
         $slider = new Slider();
         $this->saveData($request, $slider);
+        return redirect()->route('sliders.index');
+    }
+    public function edit(Slider $slider)
+    {
+        return view('dashboard.slider.edit', compact('slider'));
     }
 
     /**
      * Update the specified resource in storage.
      * @param Request $request
      * @param Slider $slider
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function update(Request $request,Slider $slider)
     {
         $this->validation($request);
         $this->saveData($request, $slider);
+        return redirect()->route('sliders.index');
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param Slider $slider
-     * @return void
+     * @return \Illuminate\Http\RedirectResponse
      * @throws \Exception
      */
     public function destroy(Slider $slider)
     {
         $slider->delete();
+        return redirect()->back();
     }
 
     /**
@@ -72,7 +85,11 @@ class SliderController extends Controller
     {
         $slider->title = $request->title;
         $slider->save();
-        $slider->image()->create(['url' => $request->image]);
+        if ($slider->image){
+            $slider->image()->update(['url' => request()->image->store('sliders','public')]);
+        }else{
+            $slider->image()->create(['url' => request()->image->store('sliders','public')]);
+        }
         $slider->save();
     }
 }
