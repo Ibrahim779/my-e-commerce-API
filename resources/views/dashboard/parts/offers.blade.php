ae62260806da6fc0fbeecfd57ffb0f754716b387
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-18 15:01:18 +0200

    Update offers.blade.php

--- resources/views/dashboard/parts/offers.blade.php
+++ resources/views/dashboard/parts/offers.blade.php
@@ -6,15 +6,22 @@
         <!-- Comment Row -->
         @forelse($offers as $offer)
         <div class="d-flex flex-row comment-row m-t-0">
-            <div class="p-2"><img style="border-radius: 0 !important;" src="../../assets/images/users/1.jpg" alt="user" width="50" class="rounded-circle"></div>
+            <div class="p-2"><img style="border-radius: 0 !important;" src="../../assets/dashboard/images/users/1.jpg" alt="user" width="50" class="rounded-circle"></div>
             <div class="comment-text w-100">
                 <h6 class="font-medium">{{$offer->name}}</h6>
                 <span class="m-b-15 d-block">{{$offer->description}} </span>
                 <div class="comment-footer">
                     <span class="text-muted float-right">{{$offer->price}}</span>
-                    <button type="button" class="btn btn-cyan btn-sm">Edit</button>
-                    <button type="button" class="btn btn-success btn-sm">Publish</button>
-                    <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                        <a href="{{route('products.removeDiscount', $offer->id)}}">
+                            <button type="button" class="btn btn-success btn-sm">
+                                remove discount
+                            </button>
+                        </a>
+                    <a href="{{route('products.destroy',  $offer->id)}}">
+                        <button type="button" class="btn btn-danger btn-sm">
+                            Delete
+                        </button>
+                    </a>
                 </div>
             </div>
         </div>
