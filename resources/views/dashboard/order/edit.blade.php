3ddac03f3b8f45c7c9b4add1d3562c6302e46d9a
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-18 14:59:56 +0200

    add orderr pages

---
+++ resources/views/dashboard/order/edit.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.dashboard')
+@section('css')
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard/extra-libs/multicheck/multicheck.css')}}">
+    <link href="{{asset('assets/dashboard/libs/datatables.net-bs4/css/dataTables.bootstrap4.css')}}" rel="stylesheet">
+@endsection
+@section('page_title','Slider| Edit')
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <form method="post" action="{{route('sliders.update', $slider->id)}}" class="form-horizontal" enctype="multipart/form-data">
+                        @method('PATCH')
+                        @csrf
+                      <div class="card-body">
+                        <h5>Slider Edit</h5>
+                          @if ($errors->any())
+                              <div class="alert alert-danger">
+                                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                      <i class="material-icons">close</i>
+                                  </button>
+                                  <span>
+                        {{$errors->first()}}
+                    </span>
+                              </div>
+                          @endif
+                        <div class="form-group row">
+                            <label for="slider_title" class="col-sm-3 text-right control-label col-form-label">SubCategory Name</label>
+                            <div class="col-sm-9">
+                                <input name="title" value="{{$slider->title}}" type="text" class="form-control" id="slider_title" placeholder="Slider Title Here">
+                            </div>
+                        </div>
+                          <div class="form-group row">
+                              <label for="category_name" class="col-sm-3 text-right control-label col-form-label">Category Image</label>
+                              <div class="col-md-9">
+                                  <div class="custom-file">
+                                      <input name="image" type="file" class="custom-file-input" id="validatedCustomFile" required>
+                                      <label  class="custom-file-label" for="validatedCustomFile">Choose category image...</label>
+                                      <div class="invalid-feedback">Example invalid custom file feedback</div>
+                                  </div>
+                              </div>
+                          </div>
+                    </div>
+                    <div class="border-top">
+                        <div class="card-body">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+@endsection
+@section('script')
+    <script src="{{asset('assets/dashboard/extra-libs/multicheck/datatable-checkbox-init.js')}}"></script>
+    <script src="{{asset('assets/dashboard/extra-libs/multicheck/jquery.multicheck.js')}}"></script>
+    <script src="{{asset('assets/dashboard/extra-libs/DataTables/datatables.min.js')}}"></script>
+    <script>
+        /****************************************
+         *       Basic Table                   *
+         ****************************************/
+        $('#zero_config').DataTable();
+    </script>
+
+@endsection
---
+++ resources/views/dashboard/order/index.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.dashboard')
+@section('css')
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard/extra-libs/multicheck/multicheck.css')}}">
+    <link href="{{asset('assets/dashboard/libs/datatables.net-bs4/css/dataTables.bootstrap4.css')}}" rel="stylesheet">
+@endsection
+@section('page_title','Orders')
+@section('content')
+ <div class="container-fluid">
+   <div class="row">
+    <div class="col-12">
+       <div class="card">
+    <div class="card-body">
+        <div class="table-responsive">
+            <table id="zero_config" class="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Phone</th>
+                    <th>Total</th>
+                    <th>Payment Status</th>
+                    <th>Order Status</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+            @foreach($orders as $order)
+                <tr>
+                    <td>{{$loop->iteration}}</td>
+                    <td>{{$order->another_name??$order->user->full_name}}</td>
+                    <td>{{$order->another_address??$order->user->address}}</td>
+                    <td>{{$order->another_phone??$order->user->phone}}</td>
+                    <td>{{$order->total_price}}</td>
+                    <td>{{$order->payment_status}}</td>
+                    <td>{{$order->status}}</td>
+                    <td>
+                        <a href="{{route('orders.show', $order->id)}}">
+                            <button type="button" class="btn btn-cyan btn-sm">show</button>
+                        </a>
+                        <a href="{{route('orders.destroy', $order->id)}}">
+                            <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+                </tbody>
+            </table>
+        </div>
+        </div>
+    </div>
+</div>
+    </div>
+</div>
+    </div>
+
+@endsection
+@section('script')
+    <script src="{{asset('assets/dashboard/extra-libs/multicheck/datatable-checkbox-init.js')}}"></script>
+    <script src="{{asset('assets/dashboard/extra-libs/multicheck/jquery.multicheck.js')}}"></script>
+    <script src="{{asset('assets/dashboard/extra-libs/DataTables/datatables.min.js')}}"></script>
+    <script>
+        /****************************************
+         *       Basic Table                   *
+         ****************************************/
+        $('#zero_config').DataTable();
+    </script>
+
+@endsection
