aa846a16aab9105c0af6e776cb79d4907956f4da
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-18 15:07:03 +0200

    Add full name attribute

--- app/User.php
+++ app/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Order::class);
     }
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
 }
