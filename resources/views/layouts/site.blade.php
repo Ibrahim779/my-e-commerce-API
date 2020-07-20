83068d82657c1bdc1906395c3602ec8d15ed8354
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-20 14:27:33 +0200

    Add site layout

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('site.includes.meta')
+    @include('site.includes.style')
 
+</head>
+<body class="goto-here">
+    @include('site.includes.nav')
+    @yield('content')
+    @include('site.includes.footer')
+    @include('site.includes.script')
+</body>
+</html>
