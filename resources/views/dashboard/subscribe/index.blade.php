c0c65219c4f696148cf33a57754bfe298900f059
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-20 18:56:37 +0200

    Create subscribe dashboard page

---
+++ resources/views/dashboard/subscribe/index.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.dashboard')
+@section('css')
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard/extra-libs/multicheck/multicheck.css')}}">
+    <link href="{{asset('assets/dashboard/libs/datatables.net-bs4/css/dataTables.bootstrap4.css')}}" rel="stylesheet">
+@endsection
+@section('page_title','Subscribes')
+
+@section('content')
+
+ <div class="container-fluid">
+
+   <div class="row">
+
+    <div class="col-12">
+
+       <div class="card">
+
+    <div class="card-body">
+
+        <div class="table-responsive">
+
+            <table id="zero_config" class="table table-striped table-bordered">
+
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Message</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+            @foreach($subscribes as $subscribe)
+                <tr>
+                    <td>{{$loop->iteration}}</td>
+                    <td>{{$message->email}}</td>
+                    <td>
+                        <form method="post" action="{{route('subscribe.destroy',  $subscribe->id)}}">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm">
+                                Delete
+                            </button>
+                        </form>
+                    </td>
+
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</div>
+    </div>
+</div>
+    </div>
+
+@endsection
+@section('script')
+    <script src="{{asset('assets/dashboard/extra-libs/multicheck/datatable-checkbox-init.js')}}"></script>
+    <script src="{{asset('assets/dashboard/extra-libs/multicheck/jquery.multicheck.js')}}"></script>
+    <script src="{{asset('assets/dashboard/extra-libs/DataTables/datatables.min.js')}}"></script>
+    <script>
+        /****************************************
+         *       Basic Table                   *
+         ****************************************/
+        $('#zero_config').DataTable();
+    </script>
+
+@endsection
