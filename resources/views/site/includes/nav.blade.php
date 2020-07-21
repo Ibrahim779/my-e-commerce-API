58e1d26d89087dc0bb23ec9729035e495634c05a
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-21 11:38:34 +0200

    Update nav.blade.php

--- resources/views/site/includes/nav.blade.php
+++ resources/views/site/includes/nav.blade.php
@@ -29,21 +29,19 @@
 
         <div class="collapse navbar-collapse" id="ftco-nav">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+                <li class="nav-item active"><a href="{{route('home.index')}}" class="nav-link">Home</a></li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shop</a>
                     <div class="dropdown-menu" aria-labelledby="dropdown04">
-                        <a class="dropdown-item" href="shop.html">Shop</a>
-                        <a class="dropdown-item" href="wishlist.html">Wishlist</a>
-                        <a class="dropdown-item" href="product-single.html">Single Product</a>
-                        <a class="dropdown-item" href="cart.html">Cart</a>
-                        <a class="dropdown-item" href="checkout.html">Checkout</a>
+                        <a class="dropdown-item" href="{{route('products.index')}}">Shop</a>
+                        <a class="dropdown-item" href="{{route('wishlist.index')}}">Wishlist</a>
+                        <a class="dropdown-item" href="{{route('cart.index')}}">Cart</a>
+                        <a class="dropdown-item" href="{{route('checkout.index')}}">Checkout</a>
                     </div>
                 </li>
-                <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-                <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-                <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-                <li class="nav-item cta cta-colored"><a href="cart.html" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
+                <li class="nav-item"><a href="{{route('about.index')}}" class="nav-link">About</a></li>
+                <li class="nav-item"><a href="{{route('contact.index')}}" class="nav-link">Contact</a></li>
+                <li class="nav-item cta cta-colored"><a href="{{route('cart.index')}}" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
 
             </ul>
         </div>
