c9ebac535b59bd1651c00ba8e21d37020e31192f
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-21 11:33:18 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::namespace('Home')->group(function (){
       Route::get('/','HomeController@index')->name('home.index');
+      Route::post('subscribes', 'SubscribeController@store')->name('subscribes.store');
+      Route::name('products.')->prefix('products')->group(function (){
+        Route::get('/','ProductController@index')->name('index');
+          Route::get('{product}','ProductController@show')->name('show');
+        Route::name('categories.')->prefix('categories')->group(function () {
+            Route::get('{category}', 'ProductController@getCategoryProducts')->name('getCategoryProducts');
+        });
+      });
+      Route::get('contact', 'ContactController@index')->name('contact.index');
+      Route::get('about', 'AboutController@index')->name('about.index');
+      Route::get('cart', 'CartController@index')->name('cart.index');
+      Route::get('wishlist', 'WishlistController@index')->name('wishlist.index');
+      Route::get('checkout', 'CheckoutController@index')->name('checkout.index');
 });
