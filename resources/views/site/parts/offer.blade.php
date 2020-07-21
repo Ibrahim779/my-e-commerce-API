3aa0e632f7e74591f6a9fde5471a387a9831652c
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-21 11:38:54 +0200

    Update offer.blade.php

--- resources/views/site/parts/offer.blade.php
+++ resources/views/site/parts/offer.blade.php
@@ -5,7 +5,7 @@
                 <span class="subheading">Best Price For You</span>
                 <h2 class="mb-4">انتظر عروضنا كل خميس</h2>
                 <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                <h3><a href="#">Shop Now</a></h3>
+                <h3><a href="{{route('products.index')}}">Shop Now</a></h3>
                 <div id="timer" class="d-flex mt-5">
                     <div class="time" id="days"></div>
                     <div class="time pl-3" id="hours"></div>
