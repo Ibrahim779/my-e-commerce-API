6112c29c334a74f079ea653749cc6a23ede2b6e5
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-21 11:35:24 +0200

    Update errors.blade.php

--- resources/views/layouts/errors.blade.php
+++ resources/views/layouts/errors.blade.php
@@ -9,19 +9,19 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/images/favicon.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/dashboard/images/favicon.png')}}">
     <title>Matrix Template - The Ultimate Multipurpose admin template</title>
     <!-- Custom CSS -->
-    <link href="{{asset('assets/dist/css/style.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/dashboard/dist/css/style.min.css')}}" rel="stylesheet">
 </head>
 
 <body>
 
   @yield('content')
 
-<script src="{{asset('assets/libs/jquery/dist/jquery.min.js')}}"></script>
-<script src="{{asset('assets/libs/popper.js/dist/umd/popper.min.js')}}"></script>
-<script src="{{asset('assets/libs/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/dashboard/libs/jquery/dist/jquery.min.js')}}"></script>
+<script src="{{asset('assets/dashboard/libs/popper.js/dist/umd/popper.min.js')}}"></script>
+<script src="{{asset('assets/dashboard/libs/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 <script>
     $('[data-toggle="tooltip"]').tooltip();
     $(".preloader").fadeOut();
