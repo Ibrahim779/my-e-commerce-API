deee0ecb9a4a48a374dd5976b1c2cd1e43dc893a
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-23 08:46:28 +0200

    Update WishlistController.php

--- app/Http/Controllers/Home/WishlistController.php
+++ app/Http/Controllers/Home/WishlistController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Wishlist;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,6 +10,20 @@ class WishlistController extends Controller
 {
     public function index()
     {
-        return view('site.wishlist.index');
+        $wishlist = Wishlist::whereUserId(1)->with('product')->get();
+        return view('site.wishlist.index', compact('wishlist'));
+    }
+    public function store($product)
+    {
+        $wishlist = new Wishlist();
+        $wishlist->product_id = $product;
+        $wishlist->user_id = 1; //Todo::make it auth()->id
+        $wishlist->save();
+        return redirect()->back();
+    }
+    public function destroy(Wishlist $wishlist)
+    {
+        $wishlist->delete();
+        return redirect()->back();
     }
 }
