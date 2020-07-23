ee8f6658762993b3bf4f68b06649fd1068f0a483
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-23 08:47:45 +0200

    Update products.blade.php

--- resources/views/site/parts/products.blade.php
+++ resources/views/site/parts/products.blade.php
@@ -14,7 +14,7 @@
                 <div class="col-md-6 col-lg-3 ftco-animate">
                     <div class="product">
                         <a href="{{route('products.show', $product->id)}}" class="img-prod">
-                            <img class="img-fluid" src="storage/{{@$product->image->url}}" alt="Colorlib Template">
+                            <img style="width: 300px;height: 200px" class="img-fluid" src="storage/{{@$product->image->url}}" alt="Colorlib Template">
                             @if($product->discount)
                                 <span class="status">{{$product->discount}}%</span>
                             @endif
@@ -41,7 +41,7 @@
                                     <a href="#" class="buy-now d-flex justify-content-center align-items-center mx-1">
                                         <span><i class="ion-ios-cart"></i></span>
                                     </a>
-                                    <a href="#" class="heart d-flex justify-content-center align-items-center ">
+                                    <a href="{{route('wishlist.store', $product->id)}}" class="heart d-flex justify-content-center align-items-center ">
                                         <span><i class="ion-ios-heart"></i></span>
                                     </a>
                                 </div>
