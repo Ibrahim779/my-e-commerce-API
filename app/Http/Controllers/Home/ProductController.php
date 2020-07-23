9721a7a0b1decffda72bb48ddadf632e2f3a063a
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-07-23 08:48:00 +0200

    Update ProductController.php

--- app/Http/Controllers/Home/ProductController.php
+++ app/Http/Controllers/Home/ProductController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Brand;
 use App\Category;
 use App\Product;
+use App\SubCategory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -13,13 +15,61 @@ class ProductController extends Controller
     {
         $products = Product::published()->get();
         $categories = Category::all();
-        return view('site.product.index', compact('products', 'categories'));
+        $subcategories = SubCategory::all();
+        $_category = null;
+        $brands = Brand::all();
+        return view('site.product.index',
+            compact('products', 'categories','subcategories','brands','_category'));
+    }
+    public function search()
+    {
+        $products = Product::published()->search(request()->search)->get();
+        $categories = Category::all();
+        $_category = null;
+        $subcategories = SubCategory::all();
+        $brands = Brand::all();
+        return view('site.product.index',
+            compact('products', 'categories','subcategories','brands', '_category'));
     }
     public function getCategoryProducts($category)
     {
         $products = Product::whereCategoryId($category)->published()->get();
         $categories = Category::all();
-        return view('site.product.index', compact('products', 'categories'));
+        $_category = Category::find($category);
+        $subcategories = SubCategory::whereCategoryId($category)->get();
+        $brands = Brand::categoriesById($category)->get();
+        return view('site.product.index',
+            compact('products', 'categories','subcategories','brands','_category'));
+    }
+    public function getSubcategoryProducts($category, $subcategory)
+    {
+        $products = Product::whereSubcategoryId($subcategory)->published()->get();
+        $categories = Category::all();
+        $_category = null;
+        $subcategories = SubCategory::whereCategoryId($category)->get();
+        $brands = Brand::categoriesById($category)->get();
+        return view('site.product.index',
+            compact('products', 'categories','subcategories','brands', '_category'));
+    }
+    public function getBrandProducts($brand)
+    {
+        $products = Product::whereBrandId($brand)->published()->get();
+        $categories = Category::all();
+        $_category = null;
+        $subcategories = SubCategory::all();
+        $brands = Brand::all();
+        return view('site.product.index',
+            compact('products', 'categories','subcategories','brands', '_category'));
+    }
+    public function getCategoryBrandProducts($category, $brand)
+    {
+        $products = Product::whereCategoryId($category)->whereBrandId($brand)->published()->get();
+        $categories = Category::all();
+        $_category = Category::find($category);
+        $subcategories = SubCategory::whereCategoryId($category)->get();
+        $brands = Brand::categoriesById($category)->get();
+        return view('site.product.index',
+            compact('products', 'categories','subcategories','brands','_category'));
     }
     public function show(Product $product)
     {
