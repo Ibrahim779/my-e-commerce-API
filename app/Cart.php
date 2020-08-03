b946a3643ab245e84e1ac3f101fb2529af8bd6fd
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-08-03 11:09:54 +0200

    Create Cart.php

---
+++ app/Cart.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    protected $guarded = [];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(user::class);
+    }
+}
