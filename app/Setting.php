9e4fcdb1adc80ca173f19c8c70f83be806900eb4
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-08-03 11:11:52 +0200

    Create Setting.php

---
+++ app/Setting.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App;
+
+
+class Setting
+{
+    public static function getInformation()
+    {
+        return [
+            'name' => 'cezar',
+            'phone' => '010125489799',
+            'address' => 'mror alsnania bad banzen eltaon',
+            'email' => 'example@example.com',
+            'website' => 'cezar.com',
+            'text' => 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+
+But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.'
+        ];
+    }
+
+
+}
