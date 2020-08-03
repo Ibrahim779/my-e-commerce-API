72bfa355cd8e9dd013ff340013108414b5ef5503
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-08-03 11:10:47 +0200

    Update cart index page

--- resources/views/site/cart/index.blade.php
+++ resources/views/site/cart/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.site')
 @section('content')
-    <div class="hero-wrap hero-bread" style="background-image: url('images/bg_1.jpg');">
+    <div class="hero-wrap hero-bread" style="background-image: url({{asset('assets/site/images/bg_1.jpg')}});">
         <div class="container">
             <div class="row no-gutters slider-text align-items-center justify-content-center">
                 <div class="col-md-9 ftco-animate text-center">
@@ -28,47 +28,60 @@
                             </tr>
                             </thead>
                             <tbody>
-                            <tr class="text-center">
-                                <td class="product-remove"><a href="#"><span class="ion-ios-close"></span></a></td>
+                            @forelse($cartItems as $cartItem)
+                                <tr class="text-center">
+                                    <td class="product-remove">
+                                        <form method="post" action="{{route('cart.destroy', $cartItem->id)}}">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit">
+                                                <span class="ion-ios-close"></span>
+                                            </button>
 
-                                <td class="image-prod"><div class="img" style="background-image:url(images/product-3.jpg);"></div></td>
+                                        </form>
 
-                                <td class="product-name">
-                                    <h3>Bell Pepper</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries</p>
-                                </td>
+                                    </td>
 
-                                <td class="price">$4.90</td>
+                                    <td class="image-prod">
+                                        <a href="{{route('products.show', @$cartItem->product->id)}}">
+                                            <div
+                                                class="img" style="background-image:url({{url('storage/'.@$cartItem->product->image->url)}});">
+                                            </div>
+                                        </a>
+                                    </td>
 
-                                <td class="quantity">
-                                    <div class="input-group mb-3">
-                                        <input type="text" name="quantity" class="quantity form-control input-number" value="1" min="1" max="100">
-                                    </div>
-                                </td>
+                                    <td class="product-name">
+                                        <a href="{{route('products.show', @$cartItem->product->id)}}">
+                                            <h3>{{@$cartItem->product->name}}</h3>
+                                        </a>
+                                        {{--                                    <p>{{$_wishlist->product->description}}</p>--}}
 
-                                <td class="total">$4.90</td>
-                            </tr><!-- END TR-->
+                                    </td>
+                                    <td class="price">
+                                        @if(@$cartItem->product->discount)
+                                            <span style="text-decoration: line-through;">{{@$cartItem->product->price}} EGY</span>  <span style="color: #ffbe08 ">{{@$cartItem->product->discount_price}} EGY</span>
+                                        @else
+                                            <span style="color: #ffbe08 ">{{@$cartItem->product->price}} EGY</span>
+                                        @endif
+                                        <span style="color: #bbb;">for {{@$cartItem->product->quantity}}</span>
+                                    </td>
 
-                            <tr class="text-center">
-                                <td class="product-remove"><a href="#"><span class="ion-ios-close"></span></a></td>
+                                    <td class="quantity">
+                                        <div class="input-group mb-3">
+                                            <input type="text" name="quantity" class="quantity form-control input-number" value="1" min="1" max="100">
+                                        </div>
+                                    </td>
 
-                                <td class="image-prod"><div class="img" style="background-image:url(images/product-4.jpg);"></div></td>
-
-                                <td class="product-name">
-                                    <h3>Bell Pepper</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries</p>
-                                </td>
-
-                                <td class="price">$15.70</td>
-
-                                <td class="quantity">
-                                    <div class="input-group mb-3">
-                                        <input type="text" name="quantity" class="quantity form-control input-number" value="1" min="1" max="100">
-                                    </div>
-                                </td>
-
-                                <td class="total">$15.70</td>
-                            </tr><!-- END TR-->
+                                    <td class="total">$4.90</td>
+                                </tr><!-- END TR-->
+                            @empty
+                                <tr class="text-center">
+                                    <td></td>
+                                    <td>
+                                        Your Cart Is Empty
+                                    </td>
+                                </tr>
+                            @endforelse
                             </tbody>
                         </table>
                     </div>
