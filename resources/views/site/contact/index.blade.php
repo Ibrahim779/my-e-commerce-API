a4a730afb47bdf1264508c805a6ed2fc22452049
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-08-03 11:12:25 +0200

    Update contact index page

--- resources/views/site/contact/index.blade.php
+++ resources/views/site/contact/index.blade.php
@@ -17,39 +17,47 @@
             <div class="w-100"></div>
             <div class="col-md-3 d-flex">
                 <div class="info bg-white p-4">
-                    <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
+                    <p><span>Address:</span> {{$information['address']}}</p>
                 </div>
             </div>
             <div class="col-md-3 d-flex">
                 <div class="info bg-white p-4">
-                    <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+                    <p><span>Phone:</span> <a href="tel://1234567920">{{$information['phone']}}</a></p>
                 </div>
             </div>
             <div class="col-md-3 d-flex">
                 <div class="info bg-white p-4">
-                    <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+                    <p><span>Email:</span> <a href="mailto:info@yoursite.com">{{$information['email']}}</a></p>
                 </div>
             </div>
             <div class="col-md-3 d-flex">
                 <div class="info bg-white p-4">
-                    <p><span>Website</span> <a href="#">yoursite.com</a></p>
+                    <p><span>Website</span> <a href="#">{{$information['website']}}</a></p>
                 </div>
             </div>
         </div>
         <div class="row block-9">
             <div class="col-md-6 order-md-last d-flex">
-                <form action="#" class="bg-white p-5 contact-form">
+                <form method="post" action="{{route('contact.store')}}" class="bg-white p-5 contact-form">
+                    @csrf
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <i class="material-icons">close</i>
+                            </button>
+                            <span>
+                                        {{$errors->first()}}
+                                    </span>
+                        </div>
+                    @endif
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Your Name">
+                        <input name="name" type="text" class="form-control" placeholder="Your Name">
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Your Email">
+                        <input name="phone" type="text" class="form-control" placeholder="Your Phone">
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Subject">
-                    </div>
-                    <div class="form-group">
-                        <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+                        <textarea name="message" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
                     </div>
                     <div class="form-group">
                         <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
