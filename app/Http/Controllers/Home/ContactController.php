09e379efbf46d4b19e54a9045ea6065f63661472
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-08-03 11:12:59 +0200

    Update ContactController.php

--- app/Http/Controllers/Home/ContactController.php
+++ app/Http/Controllers/Home/ContactController.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Message;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ContactController extends Controller
 {
     public function index()
-{
-    return view('site.contact.index');
-}
+    {
+        return view('site.contact.index');
+    }
+    public function store()
+    {
+        Message::create($this->validation());
+        return redirect()->back();
+    }
+    private function validation()
+    {
+        return \request()->validate([
+            'name' => 'required|min:3',
+            'phone' => 'required|min:11',
+            'message' => 'required|min:5'
+
+        ]);
+    }
 }
