3b1647a38b86faa21eb713e756cb9f5e4fc71efc
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-08-03 11:10:00 +0200

    Update CartController.php

--- app/Http/Controllers/Home/CartController.php
+++ app/Http/Controllers/Home/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Cart;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,6 +10,22 @@ class CartController extends Controller
 {
     public function index()
     {
-        return view('site.cart.index');
+        $cartItems = Cart::whereUserId(1)->with('product')->get();
+        return view('site.cart.index', compact('cartItems'));
+    }
+    public function store($product)
+    {
+        if (!Cart::whereProductId($product)->first()) {
+            $cart = new Cart();
+            $cart->product_id = $product;
+            $cart->user_id = 1; //Todo::make it auth()->id
+            $cart->save();
+        }
+        return back();
+    }
+    public function destroy(Cart $cart)
+    {
+        $cart->delete();
+        return back();
     }
 }
