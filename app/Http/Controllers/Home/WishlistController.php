6d68c0463476fd3fc51265c48c0503302ea28750
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-08-03 11:13:03 +0200

    Update WishlistController.php

--- app/Http/Controllers/Home/WishlistController.php
+++ app/Http/Controllers/Home/WishlistController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Product;
 use App\Wishlist;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -15,15 +16,17 @@ class WishlistController extends Controller
     }
     public function store($product)
     {
-        $wishlist = new Wishlist();
-        $wishlist->product_id = $product;
-        $wishlist->user_id = 1; //Todo::make it auth()->id
-        $wishlist->save();
-        return redirect()->back();
+        if (!Wishlist::whereProductId($product)->first()){
+            $wishlist = new Wishlist();
+            $wishlist->product_id = $product;
+            $wishlist->user_id = 1; //Todo::make it auth()->id
+            $wishlist->save();
+        }
+        return back();
     }
     public function destroy(Wishlist $wishlist)
     {
         $wishlist->delete();
-        return redirect()->back();
+        return back();
     }
 }
