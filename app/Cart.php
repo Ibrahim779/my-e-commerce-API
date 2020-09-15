6bf372a764f19f2693e439b85cefc385422c4236
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-15 23:50:00 +0200

    Update Cart.php

--- app/Cart.php
+++ app/Cart.php
@@ -16,4 +16,27 @@ class Cart extends Model
     {
         return $this->belongsTo(user::class);
     }
+    public function scopeGetUserCart($query, $user)
+    {
+        return $query->whereUserId($user)->with('product');
+    }
+    public static function getTotal()
+    {
+        //Todo:: remove static user and add auth user
+        $cart = Cart::getUserCart(1)->whereOrderId(null)->get();
+        $sub_total = 0;
+        foreach ($cart as $cartItem){
+            $sub_total += @$cartItem->product->discount_price;
+        }
+        $delivery = 5;
+        $discount = session()->get('discount') ?? 0;
+        $total = $sub_total + $delivery - $discount;
+
+        return [
+            'sub_total' => $sub_total,
+            'delivery' => $delivery,
+            'discount' => $discount,
+            'total' => $total
+        ];
+    }
 }
