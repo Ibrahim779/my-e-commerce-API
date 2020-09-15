cdacda47396e174b662ba27dc7850d7e87c83965
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-15 23:50:39 +0200

    Update CartController.php

--- app/Http/Controllers/Home/CartController.php
+++ app/Http/Controllers/Home/CartController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Home;
 
 use App\Cart;
+use App\Coupon;
+use App\Order;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,12 +12,14 @@ class CartController extends Controller
 {
     public function index()
     {
-        $cartItems = Cart::whereUserId(1)->with('product')->get();
-        return view('site.cart.index', compact('cartItems'));
+        //Todo:: remove static user and add auth user
+        $cartItems = Cart::getUserCart(1)->whereOrderId(null)->get();
+        $cart_total = Cart::getTotal();
+        return view('site.cart.index', compact('cartItems', 'cart_total'));
     }
     public function store($product)
     {
-        if (!Cart::whereProductId($product)->first()) {
+        if (!Cart::whereProductId($product)->whereOrderId(null)->first()) {
             $cart = new Cart();
             $cart->product_id = $product;
             $cart->user_id = 1; //Todo::make it auth()->id
@@ -26,6 +30,18 @@ class CartController extends Controller
     public function destroy(Cart $cart)
     {
         $cart->delete();
+        return back();
+    }
+    public function applyCoupon()
+    {
+        \request()->validate([
+            'code' => 'required'
+        ]);
+        $coupon = Coupon::whereCode(\request()->code)->first();
+        if ($coupon){
+           $discount = Cart::getTotal()['total'] * ($coupon->discount/100);
+           session()->put('discount', $discount);
+        }
         return back();
     }
 }
