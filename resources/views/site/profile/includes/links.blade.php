9d843168fe4ec74fea663cc3dc46260a8b0db248
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 09:27:36 +0200

    Create links.blade.php

---
+++ resources/views/site/profile/includes/links.blade.php
@@ -0,0 +1,14 @@
+
+<!-- External Css -->
+<link rel="stylesheet" href="{{asset('assets/site/profile/fontawesome-all.min.css')}}" as="style">
+<link rel="stylesheet" href="{{asset('assets/site/profile/themify-icons.css')}}" as="style">
+<link rel="stylesheet" href="{{asset('assets/site/profile/et-line.css')}}" as="style">
+<link rel="stylesheet" href="{{asset('assets/site/profile/bootstrap-select.min.css')}}" as="style">
+<link rel="stylesheet" href="{{asset('assets/site/profile/owl.carousel.min.css')}}" as="style">
+<!-- Custom Css -->
+<link rel="stylesheet" href="{{asset('assets/site/profile/main.css')}}" as="style" type="text/css">
+<link href="{{asset('assets/site/profile/custom.css')}}" rel="stylesheet" type="text/css">
+<!-- Fonts -->
+<link href="{{asset('assets/site/profile/css')}}" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="{{asset('assets/site/profile/dashboard.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('assets/site/profile/select2.min.css')}}">
