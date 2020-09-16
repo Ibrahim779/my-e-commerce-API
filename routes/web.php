fd1cca822f342cacad113d029098971f397cb2fc
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 18:09:28 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::namespace('Home')->group(function (){
       Route::get('/','HomeController@index')->name('home.index');
-      Route::post('subscribes', 'SubscribeController@store')->name('subscribes.store');
       Route::name('products.')->prefix('products')->group(function (){
         Route::get('/','ProductController@index')->name('index');
         Route::get('{product}','ProductController@show')->name('show');
@@ -25,20 +24,23 @@ Route::namespace('Home')->group(function (){
         });
         Route::get('brand/{brand}', 'ProductController@getBrandProducts')->name('getBrandProducts');
       });
+      Route::get('search','ProductController@search')->name('search');
+      Route::post('subscribes', 'SubscribeController@store')->name('subscribes.store');
       Route::resource('contact', 'ContactController')->only(['index', 'store']);
       Route::get('about', 'AboutController@index')->name('about.index');
-      Route::resource('wishlist', 'WishlistController')->only(['index', 'destroy']);
-      Route::get('wishlist/{product}', 'WishlistController@store')->name('wishlist.store');
-      Route::resource('cart', 'CartController')->only(['index', 'destroy']);
-      Route::post('applyCoupon', 'CartController@applyCoupon')->name('applyCoupon');
-      Route::get('cart/{product}', 'CartController@store')->name('cart.store');
-      Route::resource('checkout', 'CheckoutController')->only(['index', 'store']);
-      Route::get('search','ProductController@search')->name('search');
-      Route::get('profile/edit', 'ProfileController@edit')->name('profile.edit');
-      Route::get('profile/orders', 'ProfileController@orders')->name('profile.orders');
-    Route::get('profile/orders/show', 'ProfileController@orderShow')->name('profile.orders.show');
-
-
+      Route::middleware('auth')->group(function (){
+          Route::resource('wishlist', 'WishlistController')->only(['index', 'destroy']);
+          Route::get('wishlist/{product}', 'WishlistController@store')->name('wishlist.store');
+          Route::resource('cart', 'CartController')->only(['index', 'destroy', 'update']);
+          Route::post('cart/{product}', 'CartController@store')->name('cart.store');
+          Route::get('cart/{product}', 'CartController@store')->name('cart.store');
+          Route::post('applyCoupon', 'CartController@applyCoupon')->name('applyCoupon');
+          Route::resource('checkout', 'CheckoutController')->only(['index', 'store']);
+          Route::get('profile/edit', 'ProfileController@edit')->name('profile.edit');
+          Route::patch('profile/update', 'ProfileController@update')->name('profile.update');
+          Route::get('profile/orders', 'ProfileController@orders')->name('profile.orders');
+          Route::get('profile/orders/{order}', 'ProfileController@orderShow')->name('profile.orders.show');
+      });
 });
 
 Auth::routes();
