c06f4cf84aa2dcc7dd13792768a331baec19fb65
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 09:27:07 +0200

    Update categories.blade.php

--- resources/views/site/parts/categories.blade.php
+++ resources/views/site/parts/categories.blade.php
@@ -1,20 +1,20 @@
-<section class="ftco-section ftco-category ftco-no-pt">
+<section class="ftco-section ftco-category ftco-no-pt mt-5">
     <div class="container">
         <div class="row">
             <div class="col-md-8">
                 <div class="row">
                     <div class="col-md-6 order-md-last align-items-stretch d-flex">
-                        <div class="category-wrap-2 ftco-animate img align-self-stretch d-flex" style="background-image: url({{asset('assets/site/images/category.jpg')}});">
+                        <div class="category-wrap-2 ftco-animate img align-self-stretch d-flex" style="background-image: url({{asset('assets/site/images/categories.jpg')}});">
                             <div class="text text-center">
-                                <h2>Vegetables</h2>
-                                <p>Protect the health of every home</p>
+                                <h2>Categories</h2>
+                                <p>There are many main and sub-categories to provide easy searching for the product you need</p>
                                 <p><a href="{{route('products.index')}}" class="btn btn-primary">Shop now</a></p>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-6">
                         @foreach($categories_section1 as $category)
-                            <div class="category-wrap ftco-animate img mb-4 d-flex align-items-end" style="background-image: url({{'storage/'.@$category->image->url}});">
+                            <div class="category-wrap ftco-animate img mb-4 d-flex align-items-end" style="background-image: url({{@$category->image->url?(str_contains(@$category->image->url, 'categories')?'/storage/'.@$category->image->url:@$category->image->url):asset('assets/site/images/default.png')}});">
                                 <div class="text px-3 py-1">
                                     <h2 class="mb-0"><a href="{{route('products.categories.getCategoryProducts', $category->id)}}">{{$category->name}}</a></h2>
                                 </div>
@@ -26,7 +26,7 @@
 
             <div class="col-md-4">
                 @foreach($categories_section2 as $category)
-                    <div class="category-wrap ftco-animate img mb-4 d-flex align-items-end" style="background-image: url({{'storage/'.@$category->image->url}});">
+                    <div class="category-wrap ftco-animate img mb-4 d-flex align-items-end" style="background-image: url({{@$category->image->url?(str_contains(@$category->image->url, 'categories')?'/storage/'.@$category->image->url:@$category->image->url):asset('assets/site/images/default.png')}});">
                         <div class="text px-3 py-1">
                             <h2 class="mb-0"><a href="{{route('products.categories.getCategoryProducts', $category->id)}}">{{$category->name}}</a></h2>
                         </div>
