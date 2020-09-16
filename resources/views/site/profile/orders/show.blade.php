814d9adc1bdd0e35982423ffd5f298c24f04cd82
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 18:09:24 +0200

    Update show.blade.php

--- resources/views/site/profile/orders/show.blade.php
+++ resources/views/site/profile/orders/show.blade.php
@@ -29,34 +29,46 @@
                                     </div>
                                 </div>
                                 <div class="job-filter-result">
+                                    @forelse($cartItems as $cartItem)
                                     <div class="job-list">
                                         <div class="thumb">
-                                            <a href="https://himba.net/jobs/6" target="_blank">
-                                                <img src="./Himba _ Explore Jobs_files/15875090105e9f7712b7121.png" class="img-fluid" alt="">
+                                            <a href="{{route('products.show', $cartItem->product->id)}}">
+                                                <img src="{{@$cartItem->product->image->url?(str_contains(@$cartItem->product->image->url, 'products')?'/storage/'.@$cartItem->product->image->url:@$cartItem->product->image->url):asset('assets/site/images/default.png')}}" class="img-fluid" alt="">
                                             </a>
                                         </div>
                                         <div class="body">
                                             <div class="content">
-                                                <h4><a href="https://himba.net/jobs/6" target="_blank">Teacher for kids</a>
+                                                <h4><a href="{{route('products.show', $cartItem->product->id)}}" >{{$cartItem->product->name}}</a>
                                                 </h4>
                                                 <div class="info">
                                                     <span class="company">
-                                                          Price: <span>3500 EGY for 1K</span>
+                                                        <span style="color: #febe08">Price:</span><span>{{$cartItem->product->price}} EGY</span>
                                                     </span>
-                                                    <span class="office-location">
-                                                            Count: <span>1</span>
+                                                    <span class="company">
+                                                            <span style="color: #febe08">Quantity:</span><span>{{$cartItem->product->quantity}}</span>
+                                                    </span>
+                                                    <span class="company">
+                                                            <span style="color: #febe08">Count:</span><span>{{$cartItem->count}}</span>
+                                                    </span>
+                                                    <span class="company">
+                                                           <span style="color: #febe08">BarCode:</span><span>{{$cartItem->product->bar_code}}</span>
                                                     </span>
                                                     <span class="job-type temporary">
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock"><circle cx="12" cy="12" r="10"></circle>
                                                                 <polyline points="12 6 12 12 16 14">
                                                                 </polyline>
                                                             </svg>
-                                                            منذ يوم
+                                                            {{$cartItem->created_at->diffforhumans()}}
                                                     </span>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
+                                        @empty
+                                        <div class="job-list">
+                                            Not Found Cart Items
+                                        </div>
+                                    @endforelse
                                 </div>
 
                             </div>
