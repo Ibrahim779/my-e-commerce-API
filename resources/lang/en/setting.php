b99a4818cd211228822d1d0bcb394c9be3fbd5f7
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 19:19:04 +0200

    Create setting.php

---
+++ resources/lang/en/setting.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'site_name' => 'Brain Test',
+    'description' => 'A measure of mental abilities',
+    'newsLatter' => 'Subscribe with us',
+    'newsLatter_placeholder' => 'Subscribe with us',
+    'address' => 'Dakahlia , Egypt',
+
+];
