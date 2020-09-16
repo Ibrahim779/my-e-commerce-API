fc15bdfa6306c89262a58026747f50fb52e96d73
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 09:26:50 +0200

    Update index.blade.php

--- resources/views/site/about/index.blade.php
+++ resources/views/site/about/index.blade.php
@@ -1,20 +1,10 @@
 @extends('layouts.site')
 @section('content')
-    <div class="hero-wrap hero-bread" style="background-image: url({{asset('assets/site/images/bg_1.jpg')}});">
-        <div class="container">
-            <div class="row no-gutters slider-text align-items-center justify-content-center">
-                <div class="col-md-9 ftco-animate text-center">
-                    <p class="breadcrumbs"><span class="mr-2"><a href="{{route('home.index')}}">Home</a></span> <span>About us</span></p>
-                    <h1 class="mb-0 bread">About us</h1>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <section class="ftco-section ftco-no-pb ftco-no-pt bg-light">
+    @include('site.parts.hero', ['title' => 'About Us'])
+    <section style="background: none !important;" class="ftco-section ftco-no-pb ftco-no-pt bg-light">
         <div class="container">
             <div class="row">
-                <div class="col-md-5 p-md-5 img img-2 d-flex justify-content-center align-items-center" style="background-image: url(images/about.jpg);">
+                <div class="col-md-5 p-md-5 img img-2 d-flex justify-content-center align-items-center" style="background-image: url({{asset('assets/site/images/about_1.jpg')}});">
                     <a href="https://vimeo.com/45830194" class="icon popup-vimeo d-flex justify-content-center align-items-center">
                         <span class="icon-play"></span>
                     </a>
