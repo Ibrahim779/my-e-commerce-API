300b0d07481fc20253a24808ab13dcd5103fd37c
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 09:27:38 +0200

    Create scripts.blade.php

---
+++ resources/views/site/profile/includes/scripts.blade.php
@@ -0,0 +1,21 @@
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="{{asset('assets/site/profile/cssrelpreload.js.download')}}"></script>
+<script src="{{asset('assets/site/profile/jquery.min.js.download')}}"></script>
+
+<script src="{{asset('assets/site/profile/popper.min.js.download')}}"></script>
+<script src="{{asset('assets/site/profile/bootstrap.min.js.download')}}"></script>
+<script async="" src="{{asset('assets/site/profile/feather.min.js.download')}}"></script>
+<script src="{{asset('assets/site/profile/bootstrap-select.min.js.download')}}"></script>
+
+<script src="{{asset('assets/site/profile/owl.carousel.min.js.download')}}"></script>
+<script async="" src="{{asset('assets/site/profile/visible.js.download')}}"></script>
+<script async="" src="{{asset('assets/site/profile/jquery.countTo.js.download')}}"></script>
+
+<script src="{{asset('assets/site/profile/tinymce.min.js.download')}}"></script>
+<script src="{{asset('assets/site/profile/slick.min.js.download')}}"></script>
+<script async="" src="{{asset('assets/site/profile/custom.js.download')}}"></script>
+<script src="{{asset('assets/site/profile/dashboard.js.download')}}"></script>
+<script src="{{asset('assets/site/profile/upload-input.js.download')}}"></script>
+<script src="{{asset('assets/site/profile/jquery.validate.min.js.download')}}"></script>
+<script src="{{asset('assets/site/profile/additional-methods.min.js.download')}}"></script>
+<script src="{{asset('assets/site/profile/select2.full.min.js.download')}}"></script>
