e147a2633289c89f6f4cf6c0028057c047481377
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 19:18:46 +0200

    Create dashboard.php

---
+++ resources/lang/en/dashboard.php
@@ -0,0 +1,69 @@
+<?php
+return [
+    'dashboard' => 'Dashboard',
+    'users' => 'Users',
+    'profile' => 'Profile',
+    'messages' => 'Messages',
+    'subscribes' => 'Subscribes',
+
+    'users_count' => 'Users Count',
+    'view_all_users' => 'View all users',
+    'payed_users' => 'Payed Users',
+    'payed_success' => 'Payed success',
+    'messages_count' => 'Messages Count',
+    'view_all_messages' => 'View all messages',
+    'subscribe_count' => 'Subscribe Count',
+    'view_all_subscribes' => 'View all subscribes',
+
+    'models' => [
+        'adjective' => [
+            'name' => 'Adjectives Questions',
+            'description' => 'Here You Can Manage Adjectives Questions',
+        ],
+        'current_job' => [
+            'name' => 'Current Jobs',
+            'description' => 'Here You Can Manage Current Jobs',
+        ],
+        'future_job' => [
+            'name' => 'Future Jobs',
+            'description' => 'Here You Can Manage Future Jobs',
+        ],
+        'intelligence' => [
+            'name' => 'Intelligences',
+            'description' => 'Here You Can Manage Intelligences',
+        ],
+        'message' => [
+            'name' => 'Messages',
+            'description' => 'Here You Will Find All Messages Sent By Visitors Of Your Site',
+        ],
+        'question' => [
+            'name' => 'Questions',
+            'description' => 'Here You Can Manage Questions',
+        ],
+        'skill' => [
+            'name' => 'Skills',
+            'description' => 'Here You Can Manage Skills @ The Site - Only Super Admins Allowed',
+        ],
+        'subscribe' => [
+            'name' => 'Subscribers',
+            'description' => 'Here You Find Emails Of People Want To Subscribe Your Offers',
+        ],
+        'user' => [
+            'name' => 'Users',
+            'description' => 'Here You Can Manage Users @ The Site - Only Super Admins Allowed',
+        ],
+        'video' => [
+            'name' => 'Videos',
+            'description' => 'Here You Manage Videos On Your Site',
+        ],
+    ],
+
+    'actions' => [
+        'add' => 'Add',
+        'show' => 'Show',
+        'edit' => 'Edit',
+        'save' => 'Save',
+        'delete' => 'delete',
+    ],
+
+];
