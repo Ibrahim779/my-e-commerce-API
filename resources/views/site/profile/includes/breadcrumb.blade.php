6225966f8e38e8d08f75cb714417f7eb53bc89eb
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 09:27:33 +0200

    Create breadcrumb.blade.php

---
+++ resources/views/site/profile/includes/breadcrumb.blade.php
@@ -0,0 +1,19 @@
+<!-- Breadcrumb -->
+<div style="background: none"  class="alice-bg section-padding-to-160 padding-bottom-70">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="breadcrumb-area">
+                    <h1 style="color: #2a3d45">{{$title}}</h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="https://himba.net/employers/dashboard">Dashboard</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">{{$title}}</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumb End -->
