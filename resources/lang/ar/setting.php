ffe77f000e476e7b26933f8e5752d40817fbc56c
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 19:18:21 +0200

    Create setting.php

---
+++ resources/lang/ar/setting.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'site_name' => 'مقياس رواجبة',
+    'description' => 'مقياس رواجبة للقدرات العقلية',
+    'newsLatter' => 'اشترك معنا',
+    'newsLatter_placeholder' => 'اشترك معنا',
+    'address' => 'الدقهلية مصر',
+];
