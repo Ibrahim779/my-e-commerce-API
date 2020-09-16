bd399d110a7a639a8d92a761099ffd4ca6d88395
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 09:27:10 +0200

    Create hero.blade.php

---
+++ resources/views/site/parts/hero.blade.php
@@ -0,0 +1,10 @@
+<div class="hero-wrap hero-bread" style="background-image: url({{asset('assets/site/images/slider.jpg')}});">
+    <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+            <div class="col-md-9 ftco-animate text-center">
+                <p class="breadcrumbs"><span class="mr-2"><a style="color:#febe08;" href="{{route('home.index')}}">Home</a></span> <span style="color:#febe08;">{{$title}}</span></p>
+                <h1 style="color:#febe08;" class="mb-0 bread">{{$title}}</h1>
+            </div>
+        </div>
+    </div>
+</div>
