e04f714cdd3e11d38e32cce1c0a7b395111dc0d4
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 06:40:31 +0200

    Update unpublished_products.blade.php

--- resources/views/dashboard/parts/unpublished_products.blade.php
+++ resources/views/dashboard/parts/unpublished_products.blade.php
@@ -4,20 +4,20 @@
     </div>
     <div class="comment-widgets scrollable">
         <!-- Comment Row -->
-        @forelse($products_unpublished as $product_unpublished)
+        @forelse($products_unpublished as $product)
         <div class="d-flex flex-row comment-row m-t-0">
-            <div class="p-2"><img style="border-radius: 0 !important;" src="../../assets/dashboard/images/users/1.jpg" alt="user" width="50" class="rounded-circle"></div>
+            <div class="p-2"><img style="border-radius: 0 !important;" src="{{@$product->image->url?(str_contains(@$product->image->url, 'products')?'/storage/'.@$product->image->url:@$product->image->url):asset('assets/site/images/default.png')}}" alt="user" width="50" class="rounded-circle"></div>
             <div class="comment-text w-100">
-                <h6 class="font-medium">{{$product_unpublished->name}}</h6>
-                <span class="m-b-15 d-block">{{$product_unpublished->description}} </span>
+                <h6 class="font-medium">{{$product->name}}</h6>
+                <span class="m-b-15 d-block">{{$product->description}} </span>
                 <div class="comment-footer">
-                    <span class="text-muted float-right">{{$product_unpublished->price}}</span>
-                    <a href="{{route('products.published', $product_unpublished->id)}}">
+                    <span class="text-muted float-right">{{$product->price}}</span>
+                    <a href="{{route('products.published', $product->id)}}">
                         <button type="button" class="btn btn-success btn-sm">
                             Published
                         </button>
                     </a>
-                    <a href="{{route('products.destroy',  $product_unpublished->id)}}">
+                    <a href="{{route('products.destroy',  $product->id)}}">
                         <button type="button" class="btn btn-danger btn-sm">
                             Delete
                         </button>
