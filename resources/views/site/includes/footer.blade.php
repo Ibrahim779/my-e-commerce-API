0fc6564c1f63c45e5cc2d9f92a153d8483be4635
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 18:09:02 +0200

    Update footer.blade.php

--- resources/views/site/includes/footer.blade.php
+++ resources/views/site/includes/footer.blade.php
@@ -11,11 +11,11 @@
             <div class="col-md">
                 <div class="ftco-footer-widget mb-4">
                     <h2 class="ftco-heading-2">{{$information['name']}}</h2>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+                    <p>{{$information['sentence']}}</p>
                     <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                        <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                        <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                        <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                        <li class="ftco-animate"><a href="{{$information['facebook_link']}}"><span class="icon-twitter"></span></a></li>
+                        <li class="ftco-animate"><a href="{{$information['twitter_link']}}"><span class="icon-facebook"></span></a></li>
+                        <li class="ftco-animate"><a href="{{$information['instagram_link']}}"><span class="icon-instagram"></span></a></li>
                     </ul>
                 </div>
             </div>
@@ -32,16 +32,16 @@
             </div>
             <div class="col-md-4">
                 <div class="ftco-footer-widget mb-4">
-                    <h2 class="ftco-heading-2">Help</h2>
+                    <h2 class="ftco-heading-2">Services</h2>
                     <div class="d-flex">
                         <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
-                            <li><a href="#" class="py-2 d-block">Shipping Information</a></li>
-                            <li><a href="#" class="py-2 d-block">Returns &amp; Exchange</a></li>
-                            <li><a href="#" class="py-2 d-block">Terms &amp; Conditions</a></li>
-                            <li><a href="#" class="py-2 d-block">Privacy Policy</a></li>
+                            <li>Support</li>
+                            <li>Shipping</li>
+                            <li>Always Fresh</li>
+                            <li>Superior Quality</li>
                         </ul>
                         <ul class="list-unstyled">
-                            <li><a href="#" class="py-2 d-block">FAQs</a></li>
+                            <li>24/7 Support</li>
                             <li><a href="{{route('contact.index')}}" class="py-2 d-block">Contact</a></li>
                         </ul>
                     </div>
