5be1ab9799b0d830addec4364280f45e4646ad48
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 19:19:08 +0200

    Create site.php

---
+++ resources/lang/en/site.php
@@ -0,0 +1,67 @@
+<?php
+return [
+    'home' => 'Home',
+    'questions' => 'Questions',
+    'explanations' => 'Explanations',
+    'videos'  => 'videos',
+    'contacts' => 'Contact Us',
+    'login'    => 'Login',
+    'register' => 'Register',
+    'logout'   => 'Logout',
+
+    'present' => 'We Present',
+
+    'important' => 'Important',
+    'importance' => 'The scale is universal and its importance in personal and professional life',
+
+    'instruction' => 'Results reading directions',
+
+    'alerts'  => 'Alerts',
+    'important_alerts' => 'Important Alerts',
+
+    'question' => 'Question',
+    'answer'   => 'Answer',
+
+    'adjectives_questions' => 'Adjectives Questions',
+
+    'must_paid' => 'You Must Payed First',
+    'paid_now' => 'Pay Now',
+    'must_answer' => 'You Must Answer On Questions First',
+    'must_login' => 'You Must Login First',
+    'login_now' => 'Login Now',
+
+    'results'   => 'Results',
+    'percent'   => 'Percent',
+    'percent_chart' =>'Graph for the percentage of each pattern',
+    'skills'        =>'Skills',
+    'currentJobs'   =>'Current Jobs',
+    'futureJobs'    =>'Future Jobs',
+    'intelligence'  =>'Intelligence And Talent',
+    'video'         =>'Videos',
+    'type'          =>'Kind of intelligent',
+    'description'   =>'Characterization of intelligence',
+    'passion'       =>'Passion',
+    'development_methods'  =>'Development Methods',
+
+    'address' => 'Address',
+    'email' => 'Email',
+    'name' => 'Name',
+    'message' => 'Message',
+    'send_message' => 'Send Message',
+
+    'edit_profile' => 'Edit Profile',
+    'avatar' => 'Avatar',
+    'avatar_change' => 'Change Avatar',
+    'first_name' => 'First Name',
+    'last_name' => 'Last Name',
+    'job' => 'City',
+    'password' => 'Password',
+    'password_confirm' => 'Password Confirm',
+
+    'empty_data_message'   => 'لNo data now, you can contact us',
+    'links'                => 'Links',
+    'contact_information'  => 'Contact Information',
+    'newsLetter'           => 'NewsLetter',
+    'phone'                => 'Phone',
+
+];
