e312feb02d3ece427c07fb2ca11b7ea09656b164
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 09:26:59 +0200

    Update index.blade.php

--- resources/views/site/contact/index.blade.php
+++ resources/views/site/contact/index.blade.php
@@ -1,38 +1,28 @@
 @extends('layouts.site')
 @section('content')
-<div class="hero-wrap hero-bread" style="background-image: url({{asset('assets/site/images/bg_1.jpg')}});">
+    @include('site.parts.hero', ['title' => 'Contact'])
+<section style="background: none !important;" class="ftco-section contact-section bg-light">
     <div class="container">
-        <div class="row no-gutters slider-text align-items-center justify-content-center">
-            <div class="col-md-9 ftco-animate text-center">
-                <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Contact us</span></p>
-                <h1 class="mb-0 bread">Contact us</h1>
-            </div>
-        </div>
-    </div>
-</div>
-
-<section class="ftco-section contact-section bg-light">
-    <div class="container">
-        <div class="row d-flex mb-5 contact-info">
+        <div  class="row d-flex mb-5 contact-info">
             <div class="w-100"></div>
-            <div class="col-md-3 d-flex">
-                <div class="info bg-white p-4">
-                    <p><span>Address:</span> {{$information['address']}}</p>
+            <div  class="col-md-3 d-flex">
+                <div style="background: #febe08 !important;" class="info bg-white p-4">
+                    <p style="color:white;font-style: italic"><strong>Address :</strong> <span style="color: white">{{$information['address']}}</span></p>
                 </div>
             </div>
             <div class="col-md-3 d-flex">
-                <div class="info bg-white p-4">
-                    <p><span>Phone:</span> <a href="tel://1234567920">{{$information['phone']}}</a></p>
+                <div style="background: #febe08 !important;" class="info bg-white p-4">
+                    <p style="color:white;font-style: italic"><strong>Phone :</strong> <a style="color: white" href="tel://1234567920">{{$information['phone']}}</a></p>
                 </div>
             </div>
             <div class="col-md-3 d-flex">
-                <div class="info bg-white p-4">
-                    <p><span>Email:</span> <a href="mailto:info@yoursite.com">{{$information['email']}}</a></p>
+                <div style="background: #febe08 !important;" class="info bg-white p-4">
+                    <p style="color:white;font-style: italic"><strong>Email :</strong> <a style="color: white" href="mailto:info@yoursite.com">{{$information['email']}}</a></p>
                 </div>
             </div>
             <div class="col-md-3 d-flex">
-                <div class="info bg-white p-4">
-                    <p><span>Website</span> <a href="#">{{$information['website']}}</a></p>
+                <div style="background: #febe08 !important;" class="info bg-white p-4">
+                    <p style="color:white; font-style: italic"><strong>Website :</strong> <a style="color: white" href="#">{{$information['website']}}</a></p>
                 </div>
             </div>
         </div>
