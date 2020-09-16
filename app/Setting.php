390f9d94e2b6966e715a19344cf19b6dab0c3bb5
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 18:08:27 +0200

    Update Setting.php

--- app/Setting.php
+++ app/Setting.php
@@ -10,12 +10,17 @@ class Setting
     {
         return [
             'name' => 'cesar',
+            'sentence' => 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia',
+            'service' => '24/7 support',
             'phone' => '010125489799',
             'address' => 'mror alsnania bad banzen eltaon',
             'email' => 'example@example.com',
             'website' => 'cesar.com',
             'text' => 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
-But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.'
+               But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.',
+            'facebook_link' => '',
+            'twitter_link' => '',
+            'instagram_link' => '',
         ];
     }
 
