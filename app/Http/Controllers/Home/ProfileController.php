1d668608c5965f0b8654813fb8e357bdc3c4557c
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-16 18:08:12 +0200

    Update ProfileController.php

--- app/Http/Controllers/Home/ProfileController.php
+++ app/Http/Controllers/Home/ProfileController.php
@@ -2,21 +2,63 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Cart;
+use App\City;
+use App\Order;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
     public function edit()
     {
-        return view('site.profile.edit');
+        $cities = City::all();
+        return view('site.profile.edit', compact('cities') );
     }
     public function orders()
     {
-        return view('site.profile.orders.index');
+        $orders = Order::whereUserId(auth()->id())->get();
+        return view('site.profile.orders.index', compact('orders'));
     }
-    public function orderShow()
+    public function orderShow(Order $order)
     {
-        return view('site.profile.orders.show');
+        $cartItems = Cart::getUserCart(auth()->id())->whereOrderId($order->id)->get();
+        return view('site.profile.orders.show', compact('cartItems'));
+    }
+    public function update()
+    {
+        $this->saveData(auth()->user());
+        return back();
+    }
+    private function validation()
+    {
+        $toValidate = [
+            'first_name' => 'required|min:3|max:20',
+            'last_name' => 'required|min:3|max:20',
+            'phone' => 'required|min:3|max:255',
+            'address' => 'required|min:3|max:255',
+            'password' => 'nullable|min:8|max:255|confirmed',
+        ];
+        $toValidate['email'] = 'required|email|unique:users,id,'. auth()->user()->id;
+
+        return request()->validate($toValidate);
+    }
+    private function saveData($user)
+    {
+        $this->validation();
+        $user->first_name            = request()->first_name;
+        $user->last_name             = request()->last_name;
+        $user->email                 = request()->email;
+        $user->phone                 = request()->phone;
+        $user->city_id               = request()->city_id??null;
+        $user->address                = request()->address;
+        $user->save();
+        if ($user->image){
+            isset(request()->image)?$user->image()->update(['url' => request()->image->store('users','public')]):null;
+        }else{
+            isset(request()->image)?$user->image()->create(['url' => request()->image->store('users','public')]):null;
+        }
+        $user->save();
     }
 }
