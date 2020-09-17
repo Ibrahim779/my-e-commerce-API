60d7be0bab0ebd1ce54481078e4581c47e777132
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-18 00:00:42 +0200

    Update script.blade.php

--- resources/views/site/includes/script.blade.php
+++ resources/views/site/includes/script.blade.php
@@ -1,7 +1,10 @@
 <!-- loader -->
-<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
-
-
+<div id="ftco-loader" class="show fullscreen">
+    <svg class="circular" width="48px" height="48px">
+        <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/>
+        <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/>
+    </svg>
+</div>
 
 <script src="{{asset('assets/site/js/jquery.min.js')}}"></script>
 <script src="{{asset('assets/site/js/jquery-migrate-3.0.1.min.js')}}"></script>
@@ -21,3 +24,4 @@
 <script src="{{asset('assets/site/js/main.js')}}"></script>
 
 @yield('script')
+
