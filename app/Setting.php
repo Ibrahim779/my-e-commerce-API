7862f80a66df2beb05f555b333c3747975111329
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-17 23:59:55 +0200

    Update Setting.php

--- app/Setting.php
+++ app/Setting.php
@@ -4,20 +4,21 @@
 namespace App;
 
 
+use Illuminate\Support\Facades\App;
+
 class Setting
 {
     public static function getInformation()
     {
         return [
-            'name' => 'cesar',
-            'sentence' => 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia',
-            'service' => '24/7 support',
+            'name' => __('setting.site_name'),
+            'sentence' => __('setting.site_sentence'),
+            'service' => __('site.support_sentence'),
             'phone' => '010125489799',
-            'address' => 'mror alsnania bad banzen eltaon',
+            'address' => __('setting.site_address'),
             'email' => 'example@example.com',
             'website' => 'cesar.com',
-            'text' => 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
-               But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.',
+            'text' => __('setting.site_text'),
             'facebook_link' => '',
             'twitter_link' => '',
             'instagram_link' => '',
