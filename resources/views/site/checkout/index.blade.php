8f0fccb3c4a80667b9a7a21ab8a1f435b63578c3
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-18 00:00:28 +0200

    Update index.blade.php

--- resources/views/site/checkout/index.blade.php
+++ resources/views/site/checkout/index.blade.php
@@ -1,29 +1,29 @@
 @extends('layouts.site')
 @section('content')
-    @include('site.parts.hero', ['title' => 'Checkout'])
+    @include('site.parts.hero', ['title' => __('site.checkout')])
     <section class="ftco-section">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-xl-7 ftco-animate">
                     <form method="post" action="{{route('checkout.store')}}" class="billing-form">
                         @csrf
-                        <h3 class="mb-4 billing-heading">Billing Details</h3>
+                        <h3 class="mb-4 billing-heading">{{__('site.billing_details')}}</h3>
                         <div class="row align-items-end">
                             <div class="col-md-12">
                                 <div class="form-group">
-                                    <label for="firstname">Full Name</label>
-                                    <input value="{{old('name')??auth()->user()->full_name}}" name="name" type="text" class="form-control" placeholder="Enter Your Name">
+                                    <label for="firstname">{{__('site.full_name')}}</label>
+                                    <input value="{{old('name')??auth()->user()->full_name}}" name="name" type="text" class="form-control" placeholder="{{__('site.full_name')}}">
                                 </div>
                             </div>
 
                             <div class="w-100"></div>
                             <div class="col-md-12">
                                 <div class="form-group">
-                                    <label for="country">City</label>
+                                    <label for="country">{{__('site.city')}}</label>
                                     <div class="select-wrap">
                                         <div class="icon"><span class="ion-ios-arrow-down"></span></div>
                                         <select name="city_id" id="" class="form-control">
-                                            <option  value="{{null}}">select city</option>
+                                            <option  value="{{null}}">{{__('site.city')}}</option>
                                             @foreach($cities as $city)
                                             <option {{$city->id == auth()->user()->city_id|old('city_id')?'selected':''}} value="{{$city->id}}">{{$city->name}}</option>
                                             @endforeach
@@ -34,8 +34,8 @@
                             <div class="w-100"></div>
                             <div class="col-md-12">
                                 <div class="form-group">
-                                    <label for="streetaddress">Street Address</label>
-                                    <input value="{{old('address')??auth()->user()->address}}" name="address" type="text" class="form-control" placeholder="House number and street name">
+                                    <label for="streetaddress">{{__('site.address')}}</label>
+                                    <input value="{{old('address')??auth()->user()->address}}" name="address" type="text" class="form-control" placeholder="{{__('site.address')}}">
                                 </div>
                             </div>
 {{--                            <div class="col-md-6">--}}
@@ -47,21 +47,21 @@
                             <div class="w-100"></div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="phone">Phone</label>
-                                    <input value="{{old('phone')??auth()->user()->phone}}" name="phone" type="text" class="form-control" placeholder="Enter Your Phone">
+                                    <label for="phone">{{__('site.phone')}}</label>
+                                    <input value="{{old('phone')??auth()->user()->phone}}" name="phone" type="text" class="form-control" placeholder="{{__('site.phone')}}">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="phone">Email</label>
-                                    <input value="{{old('email')??auth()->user()->email}}" name="email" type="email" class="form-control" placeholder="Enter Your Email">
+                                    <label for="phone">{{__('site.email')}}</label>
+                                    <input value="{{old('email')??auth()->user()->email}}" name="email" type="email" class="form-control" placeholder="{{__('site.email')}}">
                                 </div>
                             </div>
                             <div class="w-100"></div>
                             <div class="col-md-12">
                                 <div class="form-group mt-4">
                                     <div class="radio">
-                                        <label class="mr-3"><input checked value="debt" type="radio" name="payment_status"> الدفع عند الاستلام  </label>
+                                        <label class="mr-3"><input checked value="debt" type="radio" name="payment_status"> {{__('site.debt')}}  </label>
 {{--                                        <label><input value="1"  type="radio" name="payment_status"> Ship to different address</label>--}}
                                     </div>
                                 </div>
@@ -69,7 +69,7 @@
                             <div class="w-100"></div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <button type="submit" class="btn btn-primary py-3 px-4">Place an order</button>
+                                    <button type="submit" class="btn btn-primary py-3 px-4">{{__('site.place_order')}}</button>
                                 </div>
                             </div>
 
@@ -80,24 +80,7 @@
                     <div class="row mt-5 pt-3">
                         <div class="col-md-12 d-flex mb-5">
                             <div class="cart-detail cart-total p-3 p-md-4">
-                                <h3 class="billing-heading mb-4">Cart Total</h3>
-                                <p class="d-flex">
-                                    <span>Subtotal</span>
-                                    <span>{{$cart_total['sub_total']}} EGY</span>
-                                </p>
-                                <p class="d-flex">
-                                    <span>Delivery</span>
-                                    <span>{{$cart_total['delivery']}} EGY</span>
-                                </p>
-                                <p class="d-flex">
-                                    <span>Discount</span>
-                                    <span>{{$cart_total['discount']}} EGY</span>
-                                </p>
-                                <hr>
-                                <p class="d-flex total-price">
-                                    <span>Total</span>
-                                    <span>{{$cart_total['total']}} EGY</span>
-                                </p>
+                               @include('site.cart.parts.total')
                             </div>
                         </div>
 {{--                        <div class="col-md-12">--}}
