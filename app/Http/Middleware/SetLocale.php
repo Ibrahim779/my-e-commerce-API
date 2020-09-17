bef47e35a4dafc939f0eb5b9e9e2fd564bcc6ba3
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-17 23:59:52 +0200

    Create SetLocale.php

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+
+class SetLocale
+{
+    public const LANG_KEY = 'lang';
+    private const DEFAULT_LANG = 'ar';
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        App::setLocale(session(self::LANG_KEY));
+        if (session(self::LANG_KEY) == self::DEFAULT_LANG)
+            session(['dir' => 'rtl']);
+        else
+            session(['dir' => 'ltr']);
+        return $next($request);
+    }
+}
