48e1da20b59a3c39732dca58a47f0429b4b91b6b
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-18 00:00:37 +0200

    Update footer.blade.php

--- resources/views/site/includes/footer.blade.php
+++ resources/views/site/includes/footer.blade.php
@@ -9,9 +9,9 @@
         </div>
         <div class="row mb-5">
             <div class="col-md">
-                <div class="ftco-footer-widget mb-4">
-                    <h2 class="ftco-heading-2">{{$information['name']}}</h2>
-                    <p>{{$information['sentence']}}</p>
+                <div style="margin-right: -5px;" class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">{{__('setting.site_name')}}</h2>
+                    <p>{{__('setting.site_sentence')}}</p>
                     <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
                         <li class="ftco-animate"><a href="{{$information['facebook_link']}}"><span class="icon-twitter"></span></a></li>
                         <li class="ftco-animate"><a href="{{$information['twitter_link']}}"><span class="icon-facebook"></span></a></li>
@@ -21,40 +21,40 @@
             </div>
             <div class="col-md">
                 <div class="ftco-footer-widget mb-4 ml-md-5">
-                    <h2 class="ftco-heading-2">Menu</h2>
+                    <h2 class="ftco-heading-2">{{__('site.menu')}}</h2>
                     <ul class="list-unstyled">
-                        <li><a href="{{route('home.index')}}" class="py-2 d-block">Home</a></li>
-                        <li><a href="{{route('about.index')}}" class="py-2 d-block">About</a></li>
-                        <li><a href="{{route('products.index')}}" class="py-2 d-block">Shop</a></li>
-                        <li><a href="{{route('contact.index')}}" class="py-2 d-block">Contact Us</a></li>
+                        <li><a href="{{route('home.index')}}" class="py-2 d-block">{{__('site.home')}}</a></li>
+                        <li><a href="{{route('about.index')}}" class="py-2 d-block">{{__('site.about')}}</a></li>
+                        <li><a href="{{route('products.index')}}" class="py-2 d-block">{{__('site.shop')}}</a></li>
+                        <li><a href="{{route('contact.index')}}" class="py-2 d-block">{{__('site.contact')}}</a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="ftco-footer-widget mb-4">
-                    <h2 class="ftco-heading-2">Services</h2>
+                    <h2  class="ftco-heading-2">{{__('site.services')}}</h2>
                     <div class="d-flex">
                         <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
-                            <li>Support</li>
-                            <li>Shipping</li>
-                            <li>Always Fresh</li>
-                            <li>Superior Quality</li>
+                            <li>{{__('site.support')}}</li>
+                            <li>{{__('site.fresh')}}</li>
+                            <li>{{__('site.shipping')}}</li>
+                            <li>{{__('site.quality')}}</li>
                         </ul>
                         <ul class="list-unstyled">
-                            <li>24/7 Support</li>
-                            <li><a href="{{route('contact.index')}}" class="py-2 d-block">Contact</a></li>
+                            <li>{{__('site.support_sentence')}}</li>
+                            <li><a href="{{route('contact.index')}}" class="py-2 d-block">{{__('site.contact')}}</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
             <div class="col-md">
                 <div class="ftco-footer-widget mb-4">
-                    <h2 class="ftco-heading-2">Have a Questions?</h2>
+                    <h2 class="ftco-heading-2">{{__('site.contact_information')}}</h2>
                     <div class="block-23 mb-3">
                         <ul>
-                            <li><span class="icon icon-map-marker"></span><span class="text">{{$information['address']}}</span></li>
-                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">{{$information['phone']}}</span></a></li>
-                            <li><a href="#"><span class="icon icon-envelope"></span><span class="text">{{$information['email']}}</span></a></li>
+                            <li><span class="icon icon-map-marker"></span> <span style="padding-right: 1px; " class="text">{{__('setting.site_address')}}</span> </li>
+                            <li><a href="#"><span class="icon icon-phone"></span> <span style="padding-right: 5px; " class="text">{{$information['phone']}}</span> </a> </li>
+                            <li><a href="#"><span class="icon icon-envelope"></span> <span style="padding-right: 5px; " class="text">{{$information['email']}}</span> </a> </li>
                         </ul>
                     </div>
                 </div>
@@ -64,7 +64,7 @@
             <div class="col-md-12 text-center">
 
                 <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> | programmed by <i class="icon-heart color-danger" aria-hidden="true"></i> <a href="https://ema-web.site" target="_blank">Ibrahim Ismail</a> | designed by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                    {{__('site.copyright')}} &copy;<script>document.write(new Date().getFullYear());</script> | {{__('site.programmed')}} <i class="icon-heart color-danger" aria-hidden="true"></i> <a href="https://ema-web.site" target="_blank">{{__('site.ibrahim_ismail')}}</a> | {{__('site.team_work')}} <a href="https://spider-te8.com" target="_blank">Spider-te8</a> | {{__('site.designed')}} <a href="https://colorlib.com" target="_blank">Colorlib</a>
                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 </p>
             </div>
