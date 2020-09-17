beb62af14d82d5ea3220ed625eea2a6af4716b62
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-18 00:01:20 +0200

    Update sidebar.blade.php

--- resources/views/site/profile/includes/sidebar.blade.php
+++ resources/views/site/profile/includes/sidebar.blade.php
@@ -4,8 +4,8 @@
             <img src="{{isset(auth()->user()->image->url)?'/storage/'.auth()->user()->image->url:asset('assets/site/images/avatar.png')}}" class="img-fluid" alt="">
         </div>
         <div class="company-body">
-            <h5 style="color: #2a3d45">ibrahim ismail</h5>
-            <span>ibrahim.ismail2022018@gmail.com</span>
+            <h5 style="color: #2a3d45">{{auth()->user()->full_name}}</h5>
+            <span>{{auth()->user()->email}}</span>
         </div>
     </div>
 
@@ -13,15 +13,15 @@
     <div  class="dashboard-menu">
         <ul >
 
-            <li class="{{$edit??''}}"><i class="ion-ios-person"></i><a  href="{{route('profile.edit')}}">Edit Profile</a></li>
-            <li class="{{$orders??''}} &quot;&quot;"><i class="ion-ios-paper"></i><a  href="{{route('profile.orders')}}">Orders</a></li>
+            <li class="{{$edit??''}}"><i class="ion-ios-person"></i><a  href="{{route('profile.edit')}}">{{__('site.edit_profile')}}</a></li>
+            <li class="{{$orders??''}} &quot;&quot;"><i class="ion-ios-paper"></i><a  href="{{route('profile.orders')}}">{{__('site.orders')}}</a></li>
         </ul>
         <ul class="delete">
             <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none;">
                 @csrf
             </form>
             <li><i class="ion-ios-power"></i><a href="{{route('logout')}}" onclick="event.preventDefault();
-                               document.getElementById(&#39;logout-form&#39;).submit();">Logout</a></li>
+                               document.getElementById(&#39;logout-form&#39;).submit();">{{__('site.logout')}}</a></li>
         </ul>
     </div>
 </div>
