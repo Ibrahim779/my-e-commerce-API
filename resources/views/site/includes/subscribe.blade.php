5ec638305c239aa363fde16c85f0b0eb0b5930e7
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-18 00:00:47 +0200

    Update subscribe.blade.php

--- resources/views/site/includes/subscribe.blade.php
+++ resources/views/site/includes/subscribe.blade.php
@@ -2,15 +2,15 @@
     <div class="container py-4">
         <div class="row d-flex justify-content-center py-5">
             <div class="col-md-6">
-                <h2 style="font-size: 22px;" class="mb-0">Subcribe to our Newsletter</h2>
-                <span>Get e-mail updates about our latest shops and special offers</span>
+                <h2 style="font-size: 22px;" class="mb-0">{{__('site.subscribe_title')}}</h2>
+                <span>{{__('site.subscribe_sentence')}}</span>
             </div>
             <div class="col-md-6 d-flex align-items-center">
                 <form method="post" action="{{route('subscribes.store')}}" class="subscribe-form">
                     @csrf
                     <div class="form-group d-flex">
-                        <input name="email" type="text" class="form-control" placeholder="Enter email address">
-                        <input type="submit" value="Subscribe" class="submit px-3">
+                        <input name="email" type="text" class="form-control" placeholder="{{__('site.email')}}">
+                        <input type="submit" value="{{__('site.subscribe')}}" class="submit px-3">
                     </div>
                 </form>
             </div>
