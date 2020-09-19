a5092b671942ef1cb7db233218071525b71f3401
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-19 19:55:56 +0200

    Update create.blade.php

--- resources/views/dashboard/subcategory/create.blade.php
+++ resources/views/dashboard/subcategory/create.blade.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" type="text/css" href="{{asset('assets/extra-libs/multicheck/multicheck.css')}}">
     <link href="{{asset('assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css')}}" rel="stylesheet">
 @endsection
-@section('page_title','SubCategory| Add')
+@section('page_title', __('site.subcategories').' | '.__('dashboard.add'))
 @section('content')
     <div class="container-fluid">
         <div class="row">
@@ -13,11 +13,11 @@
                     <form method="post" action="{{route('subcategories.categories.store', $category)}}" class="form-horizontal" enctype="multipart/form-data">
                         @csrf
                         <div class="card-body">
-                            <h5>SubCategory Create</h5>
+                            <h5>{{__('dashboard.subcategory_add')}}</h5>
                             @if ($errors->any())
                                 <div class="alert alert-danger">
                                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                        <i class="material-icons">close</i>
+                                        <i class="material-icons">{{__('site.close')}}</i>
                                     </button>
                                     <span>
                         {{$errors->first()}}
@@ -25,15 +25,15 @@
                                 </div>
                             @endif
                             <div class="form-group row">
-                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">SubCategory Name</label>
+                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">{{__('site.name')}}</label>
                                 <div class="col-sm-9">
-                                    <input name="name" value="{{old('name')}}" type="text" class="form-control" id="category_name" placeholder="Category Name Here">
+                                    <input name="name" value="{{old('name')}}" type="text" class="form-control" id="category_name" placeholder="{{__('site.name')}}">
                                 </div>
                             </div>
                         </div>
                         <div class="border-top">
                             <div class="card-body">
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" class="btn btn-primary">{{__('dashboard.submit')}}</button>
                             </div>
                         </div>
                     </form>
