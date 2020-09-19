f662e8c69ee3ec3c171a73ffb36eddc9492e2dca
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-19 19:55:46 +0200

    Update create.blade.php

--- resources/views/dashboard/product/create.blade.php
+++ resources/views/dashboard/product/create.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard/libs/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard/libs/quill/dist/quill.snow.css')}}">
 @endsection
-@section('page_title','Products| Add')
+@section('page_title', __('site.products').' | '.__('dashboard.add'))
 @section('content')
     <div class="container-fluid">
         <div class="row">
@@ -17,11 +17,11 @@
                     <form method="post" action="{{route('products.categories.store', $category)}}" class="form-horizontal" enctype="multipart/form-data">
                         @csrf
                         <div class="card-body">
-                            <h5>Products Add</h5>
+                            <h5>{{__('dashboard.product_add')}}</h5>
                             @if ($errors->any())
                                 <div class="alert alert-danger">
                                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                        <i class="material-icons">close</i>
+                                        <i class="material-icons">{{__('site.close')}}</i>
                                     </button>
                                     <span>
                         {{$errors->first()}}
@@ -29,41 +29,41 @@
                                 </div>
                             @endif
                             <div class="form-group row">
-                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">Products Name</label>
+                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">{{__('site.name')}}</label>
                                 <div class="col-sm-9">
-                                    <input name="name" value="{{old('name')}}" type="text" class="form-control" id="category_name" placeholder="Product Name Here">
+                                    <input name="name" value="{{old('name')}}" type="text" class="form-control" id="category_name" placeholder="{{__('site.name')}}">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">Products Quantity</label>
+                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">{{__('site.quantity')}}</label>
                                 <div class="col-sm-9">
-                                    <input name="quantity" value="{{old('quantity')}}" type="text" class="form-control" id="category_quantity" placeholder="Product Quantity Here">
+                                    <input name="quantity" value="{{old('quantity')}}" type="text" class="form-control" id="category_quantity" placeholder="{{__('site.quantity')}}">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">Products Price</label>
+                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">{{__('site.price')}}</label>
                                 <div class="col-sm-9">
-                                    <input name="price" value="{{old('price')}}" type="text" class="form-control" id="category_name" placeholder="Product Price Here">
+                                    <input name="price" value="{{old('price')}}" type="text" class="form-control" id="category_name" placeholder="{{__('site.price')}}">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">Products Discount %</label>
+                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">{{__('site.discount')}} %</label>
                                 <div class="col-sm-9">
-                                    <input name="discount" value="{{old('discount')}}" type="text" class="form-control" id="category_name" placeholder="Product Discount Here">
+                                    <input name="discount" value="{{old('discount')}}" type="text" class="form-control" id="category_name" placeholder="{{__('site.discount')}}">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">Products Bar Code</label>
+                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">{{__('site.bar_code')}}</label>
                                 <div class="col-sm-9">
-                                    <input name="bar_code" value="{{old('bar_code')}}" type="text" class="form-control" id="category_name" placeholder="Product Bar Code Here">
+                                    <input name="bar_code" value="{{old('bar_code')}}" type="text" class="form-control" id="category_name" placeholder="{{__('site.bar_code')}}">
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">Category Image</label>
+                                <label for="category_name" class="col-sm-3 text-right control-label col-form-label">{{__('dashboard.image')}}</label>
                                 <div class="col-md-9">
                                     <div class="custom-file">
                                         <input name="image" type="file" class="custom-file-input" id="validatedCustomFile" required>
-                                        <label  class="custom-file-label" for="validatedCustomFile">Choose category image...</label>
+                                        <label  class="custom-file-label" for="validatedCustomFile">{{__('dashboard.image')}}...</label>
                                         <div class="invalid-feedback">Example invalid custom file feedback</div>
                                     </div>
                                 </div>
@@ -73,46 +73,44 @@
                                 <div class="col-md-9">
                                     <div class="custom-control custom-checkbox mr-sm-2">
                                         <input name="is_published" type="checkbox" class="custom-control-input" id="customControlAutosizing1">
-                                        <label class="custom-control-label" for="customControlAutosizing1">Published</label>
+                                        <label class="custom-control-label" for="customControlAutosizing1">{{__('dashboard.published')}}</label>
                                     </div>
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label class="col-sm-3 text-right control-label col-form-label">SubCategory</label>
+                                <label class="col-sm-3 text-right control-label col-form-label">{{__('dashboard.subcategory')}}</label>
                                 <div class="col-md-9">
                                     <select name="subcategory_id" class="select2 form-control custom-select" style="width: 100%; height:36px;">
-                                        <option value="{{null}}">Select</option>
+                                        <option  value="{{null}}">{{__('dashboard.subcategory')}}</option>
                                         @foreach($subcategories as $subcategory)
-                                            <option value="{{$subcategory->id}}">{{$subcategory->name}}</option>
+                                            <option {{old('subcategory_id') == $subcategory->id?'selected':''}} value="{{$subcategory->id}}">{{$subcategory->name}}</option>
                                             @endforeach
                                     </select>
 
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label class="col-sm-3 text-right control-label col-form-label">Brand</label>
+                                <label class="col-sm-3 text-right control-label col-form-label">{{__('dashboard.brand')}}</label>
                                 <div class="col-md-9">
                                     <select name="brand_id" class="select2 form-control custom-select" style="width: 100%; height:36px;">
-                                        <option value="{{null}}">Select</option>
+                                        <option  value="{{null}}">{{__('dashboard.brand')}}</option>
                                         @foreach($brands as $brand)
-                                        <option value="{{$brand->id}}">{{$brand->name}}</option>
+                                        <option {{old('brand_id') == $brand->id?'selected':''}} value="{{$brand->id}}">{{$brand->name}}</option>
                                         @endforeach
                                     </select>
                                 </div>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="cono1" class="col-sm-3 text-right control-label col-form-label">Description</label>
+                            <label for="cono1" class="col-sm-3 text-right control-label col-form-label">{{__('dashboard.description')}}</label>
                             <div class="col-sm-8">
-                                    <textarea style="height: 100px" name="description" class="form-control">
-                                           {{old('description')}}
-                                    </textarea>
+                                    <textarea style="height: 100px" name="description" class="form-control" placeholder="{{__('dashboard.description')}}">{{old('description')}}</textarea>
                             </div>
                         </div>
 
                         <div class="border-top">
                             <div class="card-body">
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" class="btn btn-primary">{{__('dashboard.submit')}}</button>
                             </div>
                         </div>
                     </form>
