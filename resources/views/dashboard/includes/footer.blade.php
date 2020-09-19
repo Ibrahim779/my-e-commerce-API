38456dafeaf98931b096864c86d4ac5435c4401c
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-19 19:55:07 +0200

    Update footer.blade.php

--- resources/views/dashboard/includes/footer.blade.php
+++ resources/views/dashboard/includes/footer.blade.php
@@ -2,7 +2,10 @@
 <!-- footer -->
 <!-- ============================================================== -->
 <footer class="footer text-center">
-    All Rights Reserved by Matrix-admin. Designed and Developed by <a href="https://wrappixel.com">WrapPixel</a>.
+    <p><!-- Link back to WrapPixel can't be removed. Template is licensed under CC BY 3.0. -->
+        {{__('site.copyright')}} &copy;<script>document.write(new Date().getFullYear());</script> | {{__('site.programmed')}} <i class="icon-heart color-danger" aria-hidden="true"></i> <a href="https://ema-web.site" target="_blank">{{__('site.ibrahim_ismail')}}</a> | {{__('site.team_work')}} <a href="https://spider-te8.com" target="_blank">Spider-te8</a> | {{__('site.designed')}} <a href="https://wrappixel.com" target="_blank">WrapPixel</a>
+        <!-- Link back to WrapPixel can't be removed. Template is licensed under CC BY 3.0. -->
+    </p>
 </footer>
 <!-- ============================================================== -->
 <!-- End footer -->
