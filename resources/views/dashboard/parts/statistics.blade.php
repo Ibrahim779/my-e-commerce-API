6cf7310be9621e82a5198a275a2edada00f25345
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-19 19:55:40 +0200

    Update statistics.blade.php

--- resources/views/dashboard/parts/statistics.blade.php
+++ resources/views/dashboard/parts/statistics.blade.php
@@ -4,7 +4,7 @@
         <div class="card card-hover">
             <div class="box bg-cyan text-center">
                 <h1 class="font-light text-white"><i class="mdi mdi-view-dashboard"></i></h1>
-                <h6 class="text-white">Dashboard</h6>
+                <h6 class="text-white">{{__('dashboard.dashboard')}}</h6>
             </div>
         </div>
     </div>
@@ -13,7 +13,7 @@
         <div class="card card-hover">
             <div class="box bg-success text-center">
                 <h1 class="font-light text-white"><i>{{$products_unpublished_count}}</i></h1>
-                <h6 class="text-white">Products UnPublished</h6>
+                <h6 class="text-white">{{__('dashboard.products_unpublished')}}</h6>
             </div>
         </div>
     </div>
@@ -22,7 +22,7 @@
         <div class="card card-hover">
             <div class="box bg-warning text-center">
                 <h1 class="font-light text-white"><i>{{$messages_count}}</i></h1>
-                <h6 class="text-white">Messages</h6>
+                <h6 class="text-white">{{__('dashboard.messages')}}</h6>
             </div>
         </div>
     </div>
@@ -31,7 +31,7 @@
         <div class="card card-hover">
             <div class="box bg-danger text-center">
                 <h1 class="font-light text-white"><i>{{$pending_orders_count}}</i></h1>
-                <h6 class="text-white">Pending Order</h6>
+                <h6 class="text-white">{{__('dashboard.pending_orders')}}</h6>
             </div>
         </div>
     </div>
