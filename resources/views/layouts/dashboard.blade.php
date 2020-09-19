e2a83329767f9faf633925803ebd17733b97eb36
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-19 19:56:05 +0200

    Update dashboard.blade.php

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -20,11 +20,11 @@
         <div class="page-breadcrumb">
             <div class="row">
                 <div class="col-12 d-flex no-block align-items-center">
-                    <h4 class="page-title">@yield('page_title','Dashboard')</h4>
+                    <h4 class="page-title">@yield('page_title',__('dashboard.dashboard'))</h4>
                     <div class="ml-auto text-right">
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="{{route('dashboard.index')}}">Home</a></li>
+                                <li class="breadcrumb-item"><a href="{{route('home.index')}}">{{__('site.home')}}</a></li>
                             </ol>
                         </nav>
                     </div>
