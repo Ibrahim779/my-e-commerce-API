e0d9d4189eab32168e3884562bbc006b909adb89
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-20 21:18:53 +0200

    Update index.blade.php

--- resources/views/site/checkout/index.blade.php
+++ resources/views/site/checkout/index.blade.php
@@ -8,6 +8,16 @@
                     <form method="post" action="{{route('checkout.store')}}" class="billing-form">
                         @csrf
                         <h3 class="mb-4 billing-heading">{{__('site.billing_details')}}</h3>
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <i class="material-icons">{{__('site.close')}}</i>
+                                </button>
+                                <span>
+                        {{$errors->first()}}
+                    </span>
+                            </div>
+                        @endif
                         <div class="row align-items-end">
                             <div class="col-md-12">
                                 <div class="form-group">
