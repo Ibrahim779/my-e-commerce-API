8afb8e8f1aa73f4b8ba017c67d2e4f74278ece47
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-20 21:18:23 +0200

    Update offers.blade.php

--- resources/views/dashboard/parts/offers.blade.php
+++ resources/views/dashboard/parts/offers.blade.php
@@ -9,17 +9,21 @@
             <div class="p-2"><img style="border-radius: 0 !important;" src="{{@$offer->image->url?(str_contains(@$offer->image->url, 'products')?'/storage/'.@$offer->image->url:@$offer->image->url):asset('assets/site/images/default.png')}}" alt="user" width="50" class="rounded-circle"></div>
             <div class="comment-text w-100">
                 <h6 class="font-medium">{{$offer->name}}</h6>
+                <span class="text-muted m-b-15 d-block">{{$offer->bar_code}}</span>
                 <span class="m-b-15 d-block">{{$offer->description}} </span>
                 <div class="comment-footer">
-                    <span class="text-muted float-right">{{$offer->price}}</span>
+                    <span class="float-right" style="text-decoration: line-through;">{{$offer->price}} {{__('site.currency')}}</span> <span class="float-right" style="color: #ffbe08 ">{{$offer->discount_price}} {{__('site.currency')}}</span>
                         <a href="{{route('products.removeDiscount', $offer->id)}}">
                             <button type="button" class="btn btn-success btn-sm">
-                                remove discount
+                                {{__('dashboard.remove_discount')}}
                             </button>
                         </a>
+                    <a href="{{route('products.categories.edit', ['category' => $offer->category_id,'product' => $offer->id])}}">
+                        <button type="button" class="btn btn-cyan btn-sm">{{__('dashboard.edit')}}</button>
+                    </a>
                     <a href="{{route('products.destroy',  $offer->id)}}">
                         <button type="button" class="btn btn-danger btn-sm">
-                            Delete
+                            {{__('dashboard.delete')}}
                         </button>
                     </a>
                 </div>
