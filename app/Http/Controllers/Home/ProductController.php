5cadcdd423092f6e4bb24e688e776f1d5aa988a3
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-20 21:17:31 +0200

    Update ProductController.php

--- app/Http/Controllers/Home/ProductController.php
+++ app/Http/Controllers/Home/ProductController.php
@@ -11,10 +11,11 @@ use App\Http\Controllers\Controller;
 
 class ProductController extends Controller
 {
+    private $pagination = 50;
     public function index()
     {
-        $products = Product::published()->get();
-        $categories = Category::take(10)->get();
+        $products = Product::published()->paginate($this->pagination);
+        $categories = Category::get();
         $subcategories = SubCategory::get();
         $_category = null;
         $brands = Brand::get();
@@ -23,8 +24,8 @@ class ProductController extends Controller
     }
     public function search()
     {
-        $products = Product::published()->search(request()->search)->get();
-        $categories = Category::take(10)->get();
+        $products = Product::published()->search(request()->search)->paginate($this->pagination);
+        $categories = Category::get();
         $_category = null;
         $subcategories = SubCategory::all();
         $brands = Brand::all();
@@ -33,18 +34,28 @@ class ProductController extends Controller
     }
     public function getCategoryProducts($category)
     {
-        $products = Product::whereCategoryId($category)->published()->get();
-        $categories = Category::take(10)->get();
+        $products = Product::whereCategoryId($category)->published()->paginate($this->pagination);
+        $categories = Category::get();
         $_category = Category::find($category);
         $subcategories = SubCategory::whereCategoryId($category)->get();
         $brands = Brand::categoriesById($category)->get();
         return view('site.product.index',
             compact('products', 'categories','subcategories','brands','_category'));
     }
-    public function getSubcategoryProducts($category, $subcategory)
+    public function getSubcategoryProducts($subcategory)
     {
-        $products = Product::whereSubcategoryId($subcategory)->published()->get();
-        $categories = Category::take(10)->get();
+        $products = Product::whereSubcategoryId($subcategory)->published()->paginate($this->pagination);
+        $categories = Category::get();
+        $_category = null;
+        $subcategories = SubCategory::all();
+        $brands = Brand::all();
+        return view('site.product.index',
+            compact('products', 'categories','subcategories','brands', '_category'));
+    }
+    public function getSubcategoryProductsByCategory($category,$subcategory)
+    {
+        $products = Product::whereCategoryId($category)->whereSubcategoryId($subcategory)->published()->paginate($this->pagination);
+        $categories = Category::get();
         $_category = Category::find($category);
         $subcategories = SubCategory::whereCategoryId($category)->get();
         $brands = Brand::categoriesById($category)->get();
@@ -53,8 +64,8 @@ class ProductController extends Controller
     }
     public function getBrandProducts($brand)
     {
-        $products = Product::whereBrandId($brand)->published()->get();
-        $categories = Category::take(10)->get();
+        $products = Product::whereBrandId($brand)->published()->paginate($this->pagination);
+        $categories = Category::get();
         $_category = null;
         $subcategories = SubCategory::all();
         $brands = Brand::all();
@@ -63,8 +74,8 @@ class ProductController extends Controller
     }
     public function getCategoryBrandProducts($category, $brand)
     {
-        $products = Product::whereCategoryId($category)->whereBrandId($brand)->published()->get();
-        $categories = Category::take(10)->get();
+        $products = Product::whereCategoryId($category)->whereBrandId($brand)->published()->paginate($this->pagination);
+        $categories = Category::get();
         $_category = Category::find($category);
         $subcategories = SubCategory::whereCategoryId($category)->get();
         $brands = Brand::categoriesById($category)->get();
