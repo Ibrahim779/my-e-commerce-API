8f903f29cdb6497065d17858e4c8f93f1c0a5254
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-20 21:18:30 +0200

    Update unpublished_products.blade.php

--- resources/views/dashboard/parts/unpublished_products.blade.php
+++ resources/views/dashboard/parts/unpublished_products.blade.php
@@ -9,17 +9,25 @@
             <div class="p-2"><img style="border-radius: 0 !important;" src="{{@$product->image->url?(str_contains(@$product->image->url, 'products')?'/storage/'.@$product->image->url:@$product->image->url):asset('assets/site/images/default.png')}}" alt="user" width="50" class="rounded-circle"></div>
             <div class="comment-text w-100">
                 <h6 class="font-medium">{{$product->name}}</h6>
-                <span class="m-b-15 d-block">{{$product->description}} </span>
+                <span class="text-muted m-b-15 d-block">{{$product->bar_code}}</span>
+                <span class="m-b-15 d-block">{{$product->description}}</span>
                 <div class="comment-footer">
-                    <span class="text-muted float-right">{{$product->price}}</span>
+                    @if($product->discount)
+                    <span class="float-right" style="text-decoration: line-through;">{{$product->price}} {{__('site.currency')}}</span> <span class="float-right" style="color: #ffbe08 ">{{$product->discount_price}} {{__('site.currency')}}</span>
+                    @else
+                        <span class="float-right" style="color: #ffbe08 ">{{$product->price}} {{__('site.currency')}}</span>
+                    @endif
                     <a href="{{route('products.published', $product->id)}}">
                         <button type="button" class="btn btn-success btn-sm">
-                            Published
+                            {{$product->is_published? __('dashboard.unpublished') : __('dashboard.published')}}
                         </button>
                     </a>
+                        <a href="{{route('products.categories.edit', ['category' => $product->category_id,'product' => $product->id])}}">
+                            <button type="button" class="btn btn-cyan btn-sm">{{__('dashboard.edit')}}</button>
+                        </a>
                     <a href="{{route('products.destroy',  $product->id)}}">
                         <button type="button" class="btn btn-danger btn-sm">
-                            Delete
+                            {{__('dashboard.delete')}}
                         </button>
                     </a>
                 </div>
