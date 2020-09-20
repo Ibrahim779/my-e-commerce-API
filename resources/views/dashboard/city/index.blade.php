2cda16c13a51117e1999b039db0eee14aa9a9e13
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-09-20 09:32:38 +0200

    Update index.blade.php

--- resources/views/dashboard/city/index.blade.php
+++ resources/views/dashboard/city/index.blade.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard/extra-libs/multicheck/multicheck.css')}}">
     <link href="{{asset('assets/dashboard/libs/datatables.net-bs4/css/dataTables.bootstrap4.css')}}" rel="stylesheet">
 @endsection
-@section('page_title','Cities')
+@section('page_title', __('dashboard.cities'))
 @section('content')
  <div class="container-fluid">
    <div class="row">
@@ -16,9 +16,9 @@
                 <thead>
                 <tr>
                     <th>#</th>
-                    <th>Name</th>
-                    <th>Shipping</th>
-                    <th>Actions</th>
+                    <th>{{__('site.name')}}</th>
+                    <th>{{__('site.shipping')}}</th>
+                    <th>{{__('dashboard.action')}}</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -32,10 +32,10 @@
 
                     <td>
                         <a href="{{route('cities.edit', $city->id)}}">
-                            <button type="button" class="btn btn-cyan btn-sm">Edit</button>
+                            <button type="button" class="btn btn-cyan btn-sm">{{__('dashboard.edit')}}</button>
                         </a>
                         <a href="{{route('cities.destroy', $city->id )}}">
-                            <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                            <button type="button" class="btn btn-danger btn-sm">{{__('dashboard.delete')}}</button>
                         </a>
                     </td>
                 </tr>
@@ -43,7 +43,7 @@
                 </tbody>
             </table>
             <a href="{{route('cities.create')}}">
-                <button type="button" class="pr-5 pl-5 btn btn-cyan btn-md">Add</button>
+                <button type="button" class="pr-5 pl-5 btn btn-cyan btn-md">{{__('dashboard.add')}}</button>
             </a>
         </div>
     </div>
