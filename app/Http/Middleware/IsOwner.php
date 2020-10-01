a529a8d40464dc23512e9aa12de8f87f924ca330
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:02:11 +0200

    Create IsOwner.php

---
+++ app/Http/Middleware/IsOwner.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IsOwner
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!auth()->user()){
+            return redirect()->route('login');
+        }elseif(!auth()->user()->role == 'owner'){
+            abort(403);
+        }
+        return $next($request);
+    }
+}
