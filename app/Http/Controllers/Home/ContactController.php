e7c1eff1e4e9b759e133e5725c816d1ab2a909a5
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:01:52 +0200

    Update ContactController.php

--- app/Http/Controllers/Home/ContactController.php
+++ app/Http/Controllers/Home/ContactController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Home;
 
 use App\Message;
+use App\Traits\SaveData\MessageSaveData;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ContactController extends Controller
 {
+    use MessageSaveData;
     public function index()
     {
         return view('site.contact.index');
@@ -16,22 +18,5 @@ class ContactController extends Controller
     {
         $this->saveData(new Message());
         return back();
-    }
-    private function validation()
-    {
-        return request()->validate([
-            'name' => 'required|min:3',
-            'phone' => 'required|min:11',
-            'message' => 'required|min:5',
-        ]);
-    }
-    private function saveData(Message $message)
-    {
-        $this->validation();
-        $message->name = request()->name;
-        $message->phone = request()->phone;
-        $message->message = request()->message;
-        $message->user_id = auth()->id();
-        $message->save();
     }
 }
