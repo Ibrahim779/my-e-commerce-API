466b3cd2b410ccf6f116fbd6fbe88a6043b69591
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:05:24 +0200

    Create 2020_10_01_054108_create_settings_table.php

---
+++ database/migrations/2020_10_01_054108_create_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('key');
+            $table->text('value_en')->nullable();
+            $table->text('value_ar')->nullable();
+            $table->enum('type', ['text', 'image', 'textarea'])->default('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
