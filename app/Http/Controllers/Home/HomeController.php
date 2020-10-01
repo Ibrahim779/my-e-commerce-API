f047bb80ee6ef8ab9dc566293d94d16206e5f53e
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:01:54 +0200

    Update HomeController.php

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -16,8 +16,8 @@ class HomeController extends Controller
         $sliders = Slider::all();
         $categories_section1 = Category::inRandomOrder()->take(2)->get();
         $categories_section2 = Category::inRandomOrder()->take(2)->get();
-        $products = Product::inRandomOrder()->get();
-        $products_hasDiscount = Product::hasDiscount()->inRandomOrder()->get()->sortByDesc('discount');
+        $products = Product::inRandomOrder()->take(self::pagination)->get();
+        $products_hasDiscount = Product::hasDiscount()->inRandomOrder()->take(self::pagination)->get()->sortByDesc('discount');
         return view('site.index',compact('sliders','categories_section1','categories_section2','products','products_hasDiscount'));
     }
 }
