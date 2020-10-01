46e415bc2bbeca024a5e2c2375274488476d25df
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:09:21 +0200

    Update hero.blade.php

--- resources/views/site/parts/hero.blade.php
+++ resources/views/site/parts/hero.blade.php
@@ -2,8 +2,8 @@
     <div class="container">
         <div class="row no-gutters slider-text align-items-center justify-content-center">
             <div class="col-md-9 ftco-animate text-center">
-                <p class="breadcrumbs"><span class="mr-2"><a style="color:#febe08;" href="{{route('home.index')}}">{{__('site.home')}}</a></span> <span style="color:#febe08;">{{$title}}</span></p>
-                <h1 style="color:#febe08;" class="mb-0 bread">{{$title}}</h1>
+                <p class="breadcrumbs"><span class="mr-2"><a class="color"  href="{{route('home.index')}}">{{__('site.home')}}</a></span> <span class="color">{{$title}}</span></p>
+                <h1 class="mb-0 bread color">{{$title}}</h1>
             </div>
         </div>
     </div>
