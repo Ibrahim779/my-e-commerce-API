bb152b9ba0fdf1a594d3bf2b12fb3831f6639b92
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:01:11 +0200

    Update CategoryController.php

--- app/Http/Controllers/Dashboard/CategoryController.php
+++ app/Http/Controllers/Dashboard/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Category;
+use App\Traits\SaveData\CategorySaveData;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use mysql_xdevapi\CollectionAdd;
@@ -10,6 +11,7 @@ use phpDocumentor\Reflection\Types\Collection;
 
 class CategoryController extends Controller
 {
+    use CategorySaveData;
     /**
      * Display a listing of the resource.
      * @return mixed
@@ -60,35 +62,5 @@ class CategoryController extends Controller
     {
         $category->delete();
         return redirect()->route('categories.index');
-    }
-
-    /**
-     * @return mixed
-     */
-    private function validation()
-    {
-        return request()->validate([
-            'name' => 'required|min:3|max:150',
-            'image' => 'image|mimes:jpeg,jpg,png,gif,svg|max:10000',
-        ]);
-    }
-
-    /**
-     * @param Request $request
-     * @param $category
-     */
-    private function saveData($category)
-    {
-        $this->validation();
-        $category->name = request()->name;
-        $category->save();
-        if ($category->image){
-            if (request()->image){
-                $category->image()->update(['url' => request()->image->store('categories','public')]);
-            }
-        }else{
-            $category->image()->create(['url' => request()->image->store('categories','public')]);
-        }
-        $category->save();
     }
 }
