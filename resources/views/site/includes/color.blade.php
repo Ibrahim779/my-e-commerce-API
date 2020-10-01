3ddd409096828a3c9b8a34ad855926a60a0a2bec
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:09:12 +0200

    Create color.blade.php

---
+++ resources/views/site/includes/color.blade.php
@@ -0,0 +1,153 @@
+<style>
+    .bg-primary {
+        background: {{settings('site_color')??''}} !important;
+    }
+    .ftco-services .services:hover .icon, .ftco-services .services:focus .icon {
+        background: {{settings('site_color')??''}};
+    }
+    .btn.btn-primary {
+        background: {{settings('site_color')??''}};
+        border: 1px solid{{settings('site_color')??''}};
+    }
+    .btn.btn-primary:hover{
+        border: 1px solid {{settings('site_color')??''}};
+        color: {{settings('site_color')??''}};
+    }
+    .btn.btn-primary.btn-outline-primary{
+        border: 1px solid {{settings('site_color')??''}};
+        color: {{settings('site_color')??''}};
+    }
+    .btn.btn-primary.btn-outline-primary:hover{
+        background: {{settings('site_color')??''}};
+    }
+    .ftco-category .category-wrap-2 .text h2{
+        color: {{settings('site_color')??''}};
+    }
+    .ftco-navbar-light .navbar-brand {
+        color: {{settings('site_color')??''}};
+    }
+    .count{
+        color:white;
+        background: {{settings('site_color')??''}};
+        border-radius: 50px;
+        padding: 0 6px 0 6px;
+    }
+    .ftco-navbar-light.scrolled .nav-item.active > a {
+        color: {{settings('site_color')??''}} !important;
+    }
+    .ftco-navbar-light.scrolled .nav-item.cta > a {
+        background: {{settings('site_color')??''}} ;
+    }
+    .ftco-category .category-wrap .text {
+        background: {{settings('site_color')??''}};
+    }
+    .heading-section .subheading {
+        color: {{settings('site_color')??''}};
+    }
+    .product .text .bottom-area a {
+        background: {{settings('site_color')??''}};
+    }
+    .product .text p.price{
+        color: {{settings('site_color')??''}};
+    }
+    .product .img-prod .overlay {
+        background: {{settings('site_color')??''}};
+    }
+    a:hover, a:focus {
+        color: {{settings('site_color')??''}};
+    }
+    a {
+        color: {{settings('site_color')??''}};
+    }
+    .product .img-prod span.status {
+        background: {{settings('site_color')??''}};
+    }
+    .product .text p.price span.price-sale {
+        color: {{settings('site_color')??''}};
+    }
+    .deal-of-the-day h3 a {
+        color: {{settings('site_color')??''}};
+    }
+    #timer .time{
+        color: {{settings('site_color')??''}};
+    }
+    .subscribe-form .form-group .submit {
+        border-left: 1px solid {{settings('site_color')??''}};
+        background: {{settings('site_color')??''}} !important;
+    }
+    .mouse-icon {
+        background: {{settings('site_color')??''}};
+    }
+    .navbar-nav li:hover a {
+        color: {{settings('site_color')??''}};
+    }
+    .product-category li a.active {
+        background: {{settings('site_color')??''}};
+    }
+    .product-category li a {
+        color: {{settings('site_color')??''}};
+    }
+    .color{
+        color: {{settings('site_color')??''}}  !important;
+    }
+    .img-2 .icon {
+        background: {{settings('site_color')??''}} ;
+    }
+    .background{
+        background: {{settings('site_color')??''}} !important;
+    }
+    .dashboard-sidebar .dashboard-menu ul li:hover a, .dashboard-sidebar .dashboard-menu ul li.active a {
+        color: {{settings('site_color')??''}} !important;
+    }
+    .dashboard-sidebar .dashboard-menu ul li:hover i, .dashboard-sidebar .dashboard-menu ul li.active i {
+        color: {{settings('site_color')??''}} !important;
+    }
+    .dashboard-form .button {
+        background: {{settings('site_color')??''}};
+    }
+    .breadcrumb-area .breadcrumb li a:hover {
+        color: {{settings('site_color')??''}};
+    }
+    .table .thead-primary {
+        background: {{settings('site_color')??''}};
+    }
+    button.code{
+        border: 1px solid  {{settings('site_color')??''}} !important ;
+        color: white !important
+    }
+    .code:hover{
+        color: {{settings('site_color')??''}} !important;
+    }
+    button.add-to-cart{
+        background: {{settings('site_color')??''}} !important;
+        border: 1px solid  {{settings('site_color')??''}} !important ;
+        color: white !important;
+        text-align: center !important;
+        padding-bottom: 38px !important;
+    }
+    .add-to-cart:hover{
+        background: white !important;
+        color: {{settings('site_color')??''}} !important;
+    }
+    .dashboard-section .edit-resume {
+         background: {{settings('site_color')??''}};
+         color: white;
+     }
+    .dashboard-section .edit-resume:hover {
+        background: white;
+        color: {{settings('site_color')??''}};
+    }
+    .candidate-view-controller-wrapper .candidate-view-controller .controller.active, .candidate-view-controller-wrapper .employer-view-controller .controller.active, .candidate-view-controller-wrapper .job-view-controller .controller.active, .employer-view-controller-wrapper .candidate-view-controller .controller.active, .employer-view-controller-wrapper .employer-view-controller .controller.active, .employer-view-controller-wrapper .job-view-controller .controller.active, .job-view-controller-wrapper .candidate-view-controller .controller.active, .job-view-controller-wrapper .employer-view-controller .controller.active, .job-view-controller-wrapper .job-view-controller .controller.active {
+        color: {{settings('site_color')??''}};
+        border-color: {{settings('site_color')??''}};
+    }
+    .job-list:hover .body .content h4 {
+        color: {{settings('site_color')??''}};
+    }
+    .table tbody tr td.product-remove button:hover {
+        border: 1px solid #E10000  !important;
+        background: #E10000;
+
+    }
+
+</style>
