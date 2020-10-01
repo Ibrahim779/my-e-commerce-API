8c88fb855d7e8854fbff28c5070161f06d645b9b
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:08:19 +0200

    Create create.blade.php

---
+++ resources/views/dashboard/Settings/create.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.dashboard')
+@section('css')
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard/extra-libs/multicheck/multicheck.css')}}">
+    <link href="{{asset('assets/dashboard/libs/datatables.net-bs4/css/dataTables.bootstrap4.css')}}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard/libs/select2/dist/css/select2.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard/libs/jquery-minicolors/jquery.minicolors.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard/libs/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard/libs/quill/dist/quill.snow.css')}}">
+@endsection
+@section('page_title', __('dashboard.settings').' | '.__('dashboard.add'))
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <form method="post" action="{{route('settings.store')}}" class="form-horizontal" enctype="multipart/form-data">
+                        @csrf
+                        <div class="card-body">
+                            <h5>{{__('dashboard.setting_add')}}</h5>
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <i class="material-icons">&times;</i>
+                                    </button>
+                                    <span>
+                                        {{$errors->first()}}
+                                    </span>
+                                </div>
+                            @endif
+                            <div class="form-group row">
+                                <label for="brand_name" class="col-sm-3 text-right control-label col-form-label">{{__('site.name')}}</label>
+                                <div class="col-sm-9">
+                                    <input name="name" value="{{old('name')}}" type="text" class="form-control" id="brand_name" placeholder="{{__('site.name')}}">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="key" class="col-sm-3 text-right control-label col-form-label">{{__('dashboard.key')}}</label>
+                                <div class="col-sm-9">
+                                    <input name="key" value="{{old('key')}}" type="text" class="form-control" id="key" placeholder="{{__('dashboard.key')}}">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-3 text-right control-label col-form-label">{{__('dashboard.type')}}</label>
+                                <div class="col-md-9">
+                                    <select name="type" class="select2 form-control custom-select" style="width: 100%; height:36px;"><option  value="{{null}}">{{__('dashboard.select')}}</option>
+                                        <option {{old('type') == 'text'?'selected':''}} value="text">{{__('dashboard.text')}}</option>
+                                        <option {{old('type') == 'image'?'selected':''}} value="image">{{__('dashboard.image')}}</option>
+                                        <option {{old('type') == 'textarea'?'selected':''}} value="textarea">{{__('dashboard.textarea')}}</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="border-top">
+                            <div class="card-body">
+                                <button type="submit" class="btn btn-primary">{{__('dashboard.submit')}}</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+@endsection
+@section('script')
+    <script src="{{asset('assets/dashboard/extra-libs/multicheck/datatable-checkbox-init.js')}}"></script>
+    <script src="{{asset('assets/dashboard/extra-libs/multicheck/jquery.multicheck.js')}}"></script>
+    <script src="{{asset('assets/dashboard/extra-libs/DataTables/datatables.min.js')}}"></script>
+    <script src="{{asset('assets/dashboard/libs/inputmask/dist/min/jquery.inputmask.bundle.min.js')}}"></script>
+    <script src="{{asset('assets/dashboard/dist/js/pages/mask/mask.init.js')}}"></script>
+    <script src="{{asset('assets/dashboard/libs/select2/dist/js/select2.full.min.js')}}"></script>
+    <script src="{{asset('assets/dashboard/libs/select2/dist/js/select2.min.js')}}"></script>
+    <script src="{{asset('assets/dashboard/libs/jquery-asColor/dist/jquery-asColor.min.js')}}"></script>
+    <script src="{{asset('assets/dashboard/libs/jquery-asGradient/dist/jquery-asGradient.js')}}"></script>
+    <script src="{{asset('assets/dashboard/libs/jquery-asColorPicker/dist/jquery-asColorPicker.min.js')}}"></script>
+    <script src="{{asset('assets/dashboard/libs/jquery-minicolors/jquery.minicolors.min.js')}}"></script>
+    <script src="{{asset('assets/dashboard/libs/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
+    <script src="{{asset('assets/dashboard/libs/quill/dist/quill.min.js')}}"></script>
+    <script>
+        //***********************************//
+        // For select 2
+        //***********************************//
+        $(".select2").select2();
+
+        /*colorpicker*/
+        $('.demo').each(function() {
+            //
+            // Dear reader, it's actually very easy to initialize MiniColors. For example:
+            //
+            //  $(selector).minicolors();
+            //
+            // The way I've done it below is just for the demo, so don't get confused
+            // by it. Also, data- attributes aren't supported at this time...they're
+            // only used for this demo.
+            //
+            $(this).minicolors({
+                control: $(this).attr('data-control') || 'hue',
+                position: $(this).attr('data-position') || 'bottom left',
+
+                change: function(value, opacity) {
+                    if (!value) return;
+                    if (opacity) value += ', ' + opacity;
+                    if (typeof console === 'object') {
+                        console.log(value);
+                    }
+                },
+                theme: 'bootstrap'
+            });
+
+        });
+        /*datwpicker*/
+        jQuery('.mydatepicker').datepicker();
+        jQuery('#datepicker-autoclose').datepicker({
+            autoclose: true,
+            todayHighlight: true
+        });
+        var quill = new Quill('#editor', {
+            theme: 'snow'
+        });
+
+    </script>
+
+@endsection
