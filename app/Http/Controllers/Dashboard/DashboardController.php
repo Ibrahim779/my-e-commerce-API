4c23554ae32850de512b3d2ef1c7d3fdb894170d
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:01:19 +0200

    Update DashboardController.php

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -13,11 +13,11 @@ class DashboardController extends Controller
     public function index()
     {
         $products_unpublished_count = Product::unpublished()->count();
-        $offers = Product::hasDiscount()->take(4)->get();
-        $products_unpublished = Product::unpublished()->take(4)->get();
+        $offers = Product::hasDiscount()->take(10)->get();
+        $products_unpublished = Product::unpublished()->take(10)->get();
         $pending_orders =  Order::pending()->get();
         $pending_orders_count =  Order::pending()->count();
-        $latest_messages = Message::latest('created_at')->take(4)->get();
+        $latest_messages = Message::latest('created_at')->take(10)->get();
         $messages_count = Message::count();
         return view('dashboard.index',compact(
             'products_unpublished_count',
