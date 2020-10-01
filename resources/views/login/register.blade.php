cacbd80f4e68d58c69162a4a18223fd6f421aae8
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:09:02 +0200

    Update register.blade.php

--- resources/views/login/register.blade.php
+++ resources/views/login/register.blade.php
@@ -17,7 +17,7 @@
                     @if ($errors->any())
                         <div class="alert alert-danger">
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <i class="fa fa-times"></i>
+                                <i class="fa fa-times">&times;</i>
                             </button>
                             <span>
                         {{$errors->first()}}
