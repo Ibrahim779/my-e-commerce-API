b65775c2a5b58a7f583fbb2ceddfdb0679d3af10
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:09:16 +0200

    Update footer.blade.php

--- resources/views/site/includes/footer.blade.php
+++ resources/views/site/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="ftco-footer ftco-section">
     <div class="container">
         <div class="row">
-            <div class="mouse">
+            <div style="text-align: center !important;" class="mouse">
                 <a href="#" class="mouse-icon">
                     <div class="mouse-wheel"><span class="ion-ios-arrow-up"></span></div>
                 </a>
