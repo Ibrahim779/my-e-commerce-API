f40ce2da9098c8bb29b128236c8692bcd113ce83
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:03:59 +0200

    Update User.php

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'first_name', 'last_name', 'phone', 'city_id', 'address', 'email', 'password',
+        'first_name', 'last_name', 'phone', 'city_id', 'address', 'role', 'email', 'password',
     ];
 
     /**
