4810bce4e92c8cb0ce94075272602af7b7e56933
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:02:26 +0200

    Create SiteSetting.php

---
+++ app/SiteSetting.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App;
+
+
+use Illuminate\Support\Facades\App;
+
+class SiteSetting
+{
+    public static function getInformation()
+    {
+        return [
+            'name' => __('setting.site_name'),
+            'sentence' => __('setting.site_sentence'),
+            'service' => __('site.support_sentence'),
+            'phone' => '010125489799',
+            'address' => __('setting.site_address'),
+            'email' => 'example@example.com',
+            'website' => 'cesar.com',
+            'text' => __('setting.site_text'),
+            'facebook_link' => '',
+            'twitter_link' => '',
+            'instagram_link' => '',
+        ];
+    }
+
+
+}
