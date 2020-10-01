a6f0018311147d62780ae40909d9bea58a9e0970
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:07:17 +0200

    Update setting.php

--- resources/lang/en/setting.php
+++ resources/lang/en/setting.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'site_name' => 'Cesar',
+    'site_name' => settings('site_name', 'en'),
     'site_sentence' => 'Hyper Cesar Will Provide You All Things That You need',
     'site_text' => 'Hyper Cesar Will Provide You All Things That You need',
     'site_address' => 'Damietta, beside Al-Senaniya traffic after Al-Matbouli Furniture',
