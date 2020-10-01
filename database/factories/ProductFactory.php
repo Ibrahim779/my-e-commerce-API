e23b4934d01e0bc6f99c02ae7f20bf46592b50ae
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2020-10-01 21:04:14 +0200

    Update ProductFactory.php

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -12,12 +12,11 @@ $factory->define(Product::class, function (Faker $faker) {
         'price' => $faker->randomFloat(10),
         'description' => $faker->text,
         'discount' => $faker->randomElement([$faker->numberBetween(5, 90), null]),
-        'quantity' => $faker->randomElement(['1K', '1p', '1 علبة']),
+        'quantity' => $faker->randomElement(['1K', '4k', '1 علبة']),
         'bar_code' => Str::random(10),
         'category_id' => factory(\App\Category::class),
         'brand_id' => factory(\App\Brand::class),
         'subcategory_id' => factory(\App\SubCategory::class),
-        'is_published' => $faker->randomElement(['on', null]),
-        'is_offer' => $faker->randomElement(['on', null])
+        'is_published' => $faker->randomElement(['on', null])
     ];
 });
